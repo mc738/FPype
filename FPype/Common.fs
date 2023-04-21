c2e1a536e027e17668efdde8e04a2c838acc1072
Max Clifford <me@mclifford.dev>
2023-04-21 22:26:36 +0100

    Removed comments

--- FPype/Common.fs
+++ FPype/Common.fs
@@ -10,101 +10,6 @@ open FPype.Data.Models
 open FPype.Data.Store
 open FsToolbox.Core
 
-(*
-module Serialization =
-
-    let getData (element: JsonElement) =
-        Json.tryGetElementsProperty "data" element |> Option.map Json.propertiesToMap
-
-    let createField (element: JsonElement) =
-        let name = Json.tryGetStringProperty "name" element
-
-        let fieldType =
-            Json.tryGetStringProperty "type" element
-            |> Option.bind (fun t ->
-                BaseType.FromId(t, Json.tryGetBoolProperty "optional" element |> Option.defaultValue false))
-
-        //TODO import handler.
-
-        match name, fieldType with
-        | Some n, Some ft ->
-            Ok(
-                { Name = n
-                  Type = ft
-                  ImportHandler = None }: TableColumn
-            )
-        | None, _ -> Error "Missing name property."
-        | _, None -> Error "Failed to create column type."
-
-    let createFields (elements: JsonElement list) =
-        elements
-        |> List.map createField
-        |> List.fold
-            (fun (r, e) tc ->
-                match tc with
-                | Ok c -> r @ [ c ], e
-                | Error em -> r, e @ [ em ])
-            ([], [])
-        |> fun (tcs, e) ->
-            match e |> List.isEmpty with
-            | true -> Ok tcs
-            | false ->
-                let errors = e |> String.concat " "
-                Error $"Failed to create table columns. Errors: {errors}"
-
-    let createTable (element: JsonElement) =
-        let name = Json.tryGetStringProperty "name" element
-
-        let fields =
-            Json.tryGetArrayProperty "fields" element
-            |> Option.map createFields
-            |> Option.defaultValue (Error "Missing fields property")
-
-        match name, fields with
-        | Some n, Ok f -> Ok(n, f)
-        | None, _ -> Error "Missing name property"
-        | _, Error e -> Error e
-
-    let createOutputTable (data: Map<string, JsonElement>) =
-        data.TryFind "outputTable"
-        |> Option.map createTable
-        |> Option.defaultValue (Error "Missing outputTable property")
-
-    let createImportAction (data: Map<string, JsonElement>) =
-        let path = data.TryFind "path" |> Option.map (fun el -> el.GetString())
-
-        let name = data.TryFind "name" |> Option.map (fun el -> el.GetString())
-
-        match path, name with
-        | Some p, Some n -> Import.file p n |> Ok
-        | None, _ -> Error $"Missing path value in action data."
-        | _, None -> Error "Missing name value in action data."
-
-    let createParseCsvAction (data: Map<string, JsonElement>) =
-        let source = data.TryFind "source" |> Option.map (fun el -> el.GetString())
-
-        match source, createOutputTable data with
-        | Some s, Ok (n, tc) -> Extract.parseCsv s tc n |> Ok
-        | None, _ -> Error $"Missing path value in action data."
-        | _, Error e -> Error e
-
-    let createAggregateAction (data: Map<string, JsonElement>) =
-
-
-
-        ()
-
-    let createAction (element: JsonElement) : Result<PipelineStore -> Result<PipelineStore, string>, string> =
-        Json.tryGetStringProperty "action" element
-        |> Option.map (fun action ->
-            match action, getData element with
-            | "import", Some data -> createImportAction data
-            | "parseCsv", Some data -> createParseCsvAction data
-            | _, Some _ -> Error $"Unknown action: {action}"
-            | _, None -> Error "Missing data property.")
-        |> Option.defaultValue (Error "Missing `action` property.")
-*)
-
 type PipelineContext =
     { Id: string
       StorePath: string
@@ -134,19 +39,6 @@ type PipelineContext =
                   Store = store
                   LogToConsole = logToConsole
                   Actions = actions })
-
-        // Add some basic values for use in later steps (if needed).
-        // TODO make a separate context table for these?
-        //store.SetId(id)
-        //store.SetComputerName()
-
-        //store.AddStateValue("__id", id)
-        //store.AddStateValue("__computer_name", Environment.MachineName)
-        //store.AddStateValue("__user_name", Environment.UserName)
-        //store.AddStateValue("__base_path", dir)
-        //store.AddStateValue("__imports_path", importDir)
-        //store.AddStateValue("__exports_path", exportDir)
-        //store.AddStateValue("__tmp_value", tmpDir)
 
         | false -> Error $"Base directory `{basePath}` does not exist."
 
