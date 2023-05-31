ab40e56964e3a591750a5d5c8a7de148e78c18fd
Max Clifford <me@mclifford.dev>
2023-05-31 23:41:13 +0100

    Added SecureValue

---
+++ FPype/Security/Common.fs
@@ -0,0 +1,32 @@
+﻿namespace FPype.Security
+
+open System
+open System.Text.Json
+open FsToolbox.Core
+
+[<AutoOpen>]
+module Common =
+    
+    [<RequireQualifiedAccess>]
+    type SecureValue =
+        | EnvironmentVariable of Variable: string
+  
+        static member FromJson(json: JsonElement) =
+            match Json.tryGetStringProperty "type" json with
+            | Some "environment-variable"
+            | Some "environment_variable" ->
+                match Json.tryGetStringProperty "value" json with
+                | Some v -> EnvironmentVariable v |> Ok
+                | None -> Error "Missing `value` property"
+            | Some t -> Error $"Unknown secure value type: `{t}`"
+            | None -> Error $"Missing `type` property"
+        
+        member sv.Resolve() =
+            match sv with
+            | EnvironmentVariable variable -> Environment.GetEnvironmentVariable variable
+       
+    
+    
+    ()
+    
+
--- FPype/Security/Credentials.fs
+++ FPype/Security/Credentials.fs
@@ -2,5 +2,6 @@
 
 module Credentials =
     
+    
     ()
 
