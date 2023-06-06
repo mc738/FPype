03a1df8e66a3c1733d5e5c10dd56685e9d783594
Max Clifford <me@mclifford.dev>
2023-06-06 23:49:06 +0100

    Config builder almost done

--- FPype.Infrastructure/Configuration/Impl.fs
+++ FPype.Infrastructure/Configuration/Impl.fs
@@ -1,5 +1,6 @@
 ﻿namespace FPype.Infrastructure.Configuration
 
+open Armarium
 open FPype.Configuration
 open FPype.Infrastructure.Configuration.Common
 open FPype.Infrastructure.Configuration.Tables
@@ -22,19 +23,19 @@ module Impl =
             Operations.selectTableModelVersionRecords ctx [ "WHERE table_model_id = @0" ] [ tm.Id ]
             |> List.map (fun tv ->
                 Tables.StoreOperations.addTableVersion ctx cfg subscription tv.Reference
-                
+
                 let tcs =
                     Operations.selectTableColumnRecords ctx [ "WHERE table_model_version = @0" ] [ tv.Id ]
-                    |> List.map (fun tc -> ({
-                        Id = IdType.Specific tc.Reference
-                        Name = tc.Name
-                        DataType = tc.DataType
-                        Optional = tc.Optional
-                        ImportHandler = tc.ImportHandlerJson 
-                    }: Tables.NewColumn))
-                    
+                    |> List.map (fun tc ->
+                        ({ Id = IdType.Specific tc.Reference
+                           Name = tc.Name
+                           DataType = tc.DataType
+                           Optional = tc.Optional
+                           ImportHandler = tc.ImportHandlerJson }
+                        : Tables.NewColumn))
+
                 cfg.AddTableVersion(IdType.Specific tv.Reference, tm.Name, tcs, ItemVersion.Specific tv.Version))
-            
+
         let addQuery
             (ctx: MySqlContext)
             (cfg: ConfigurationStore)
@@ -46,50 +47,55 @@ module Impl =
                 // Double fetch?
                 Queries.StoreOperations.addQueryVersion ctx cfg subscription qv.Reference)
 
-    let buildNewStore (ctx: MySqlContext) (subscription: string) (path: string) (additionActions: string list) =
+    let buildNewStore
+        (ctx: MySqlContext)
+        (fileRepo: FileRepository)
+        (readArgs: FileReadOperationArguments)
+        (subscription: string)
+        (path: string)
+        (failOnError: bool)
+        (additionActions: string list)
+        =
         // No need to check serials when creating a fresh store.
         // Get tip serial however
         Fetch.subscriptionByReference ctx subscription
-        |> FetchResult.toResult
-        |> Result.map (fun sub ->
+        |> FetchResult.toActionResult
+        |> ActionResult.bind (fun sub ->
             // A transaction is used to "freeze" the database.
             // This might not be the best idea (?) but it is to ensure no updates happen while building the config
-            ctx.ExecuteInTransaction(fun t ->
 
-                let tip = Events.selectGlobalTip
+            let result =
+                ctx.ExecuteInTransaction(fun t ->
 
-                let metadata =
-                    [ "subscription_id", subscription; "serial_tip", string tip ] |> Map.ofList
+                    let tip = Events.selectGlobalTip
 
-                let cfg = ConfigurationStore.Initialize(path, additionActions, metadata)
+                    let metadata =
+                        [ "subscription_id", subscription; "serial_tip", string tip ] |> Map.ofList
 
-                // Add tables
-                Tables.StoreOperations.addTableVersion
-                                
-                
-                
+                    let cfg = ConfigurationStore.Initialize(path, additionActions, metadata)
 
-                
-                let tables =
-                    Operations.selectTableModelRecords t [ "WHERE subscription_id = @0" ] [ sub.Id ]
-                    |> List.map (fun tm ->
-                        Operations.selectTableModelVersionRecords t [ "WHERE table_model_id = @0" ] [ tm.Id ]
-                        |> List.map (fun tv ->
-                            let tcs =
-                                Operations.selectTableColumnRecords t [ "WHERE table_model_version = @0" ] [ tv.Id ]
-                            ())
-
-
-
+                    // Add tables
+                    Tables.StoreOperations.addAllTableVersions t failOnError sub cfg
+                    |> ActionResult.bind (Queries.StoreOperations.addAllQueryVersions t failOnError sub)
+                    |> ActionResult.bind (
+                        Resources.StoreOperations.addAllResourceVersions t fileRepo readArgs failOnError sub
                     )
+                    |> ActionResult.bind (
+                        TableObjectMappers.StoreOperations.addAllTableObjectMapperVersions t failOnError sub
+                    )
+                    |> ActionResult.bind (
+                        ObjectTableMappers.StoreOperations.addAllObjectTableMapperVersions t failOnError sub
+                    ))
 
-
-                ()))
-
-
-
-
-
+            match result with
+            | Ok ar -> ar
+            | Error e ->
+                ({ Message = e
+                   DisplayMessage = "Failed to create store"
+                   Exception = None }
+                : FailureResult)
+                |> ActionResult.Failure)
+        
     let buildStoreFromSerial () = ()
 
 
