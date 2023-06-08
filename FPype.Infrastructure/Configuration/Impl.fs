dc5b6e454016b0706205e00690b7bfe9ad31ff1d
Max Clifford <me@mclifford.dev>
2023-06-08 23:49:09 +0100

    Work on conf builder

--- FPype.Infrastructure/Configuration/Impl.fs
+++ FPype.Infrastructure/Configuration/Impl.fs
@@ -1,5 +1,6 @@
 ﻿namespace FPype.Infrastructure.Configuration
 
+open System
 open FPype.Infrastructure.Configuration.Common.Events
 open FPype.Infrastructure.Core.Persistence
 
@@ -31,26 +32,23 @@ module Impl =
                 Pipelines.StoreOperations.addPipelineAction ctx store subscription data.Reference
             | PipelineResourceAdded data ->
                 Pipelines.StoreOperations.addPipelineResource ctx store subscription data.Reference
-            | PipelineArgAdded data ->
-                Pipelines.StoreOperations.addPipelineArg ctx store subscription data.Reference
-            | TableAdded data ->
-                Tables.StoreOperations.addTable ctx store subscription data.Reference
-            | TableVersionAdded data ->
-                Tables.StoreOperations.addTableVersion ctx store subscription data.Reference
-            | TableColumnAdded data -> 
-                Tables.StoreOperations.addTableColumn ctx store subscription data.Reference
-            | QueryAdded data ->
-                Queries.StoreOperations.addQuery ctx store subscription data.Reference
-            | QueryVersionAdded data ->
-                Queries.StoreOperations.addQueryVersion ctx store subscription data.Reference
-            | ResourceAdded data ->
-                Resources.StoreOperations.addResource ctx store subscription data.Reference
-            | ResourceVersionAdded data -> failwith "todo"
-            | TableObjectMapperAdded data -> failwith "todo"
-            | TableObjectMapperVersionAdded data -> failwith "todo"
-            | ObjectTableMapperAdded data -> failwith "todo"
-            | ObjectTableMapperVersionAdded data -> failwith "todo"
-
+            | PipelineArgAdded data -> Pipelines.StoreOperations.addPipelineArg ctx store subscription data.Reference
+            | TableAdded data -> Tables.StoreOperations.addTable ctx store subscription data.Reference
+            | TableVersionAdded data -> Tables.StoreOperations.addTableVersion ctx store subscription data.Reference
+            | TableColumnAdded data -> Tables.StoreOperations.addTableColumn ctx store subscription data.Reference
+            | QueryAdded data -> Queries.StoreOperations.addQuery ctx store subscription data.Reference
+            | QueryVersionAdded data -> Queries.StoreOperations.addQueryVersion ctx store subscription data.Reference
+            | ResourceAdded data -> Resources.StoreOperations.addResource ctx store subscription data.Reference
+            | ResourceVersionAdded data ->
+                Resources.StoreOperations.addResourceVersion ctx store fileRepo readArgs subscription data.Reference
+            | TableObjectMapperAdded data ->
+                TableObjectMappers.StoreOperations.addTableObjectMapper ctx store subscription data.Reference
+            | TableObjectMapperVersionAdded data ->
+                TableObjectMappers.StoreOperations.addTableObjectMapperVersion ctx store subscription data.Reference
+            | ObjectTableMapperAdded data ->
+                ObjectTableMappers.StoreOperations.addObjectTableMapper ctx store subscription data.Reference
+            | ObjectTableMapperVersionAdded data ->
+                ObjectTableMappers.StoreOperations.addObjectTableMapperVersion ctx store subscription data.Reference
 
     let buildNewStore
         (ctx: MySqlContext)
@@ -72,7 +70,7 @@ module Impl =
             let result =
                 ctx.ExecuteInTransaction(fun t ->
 
-                    let tip = Events.selectGlobalTip
+                    let tip = Events.selectGlobalTip t
 
                     let metadata =
                         [ "subscription_id", subscription; "serial_tip", string tip ] |> Map.ofList
@@ -101,22 +99,43 @@ module Impl =
                 : FailureResult)
                 |> ActionResult.Failure)
 
-    let buildStoreFromSerial () = ()
-
-
-
-    let createConfigurationStore
+    let buildStoreFromSerial
         (ctx: MySqlContext)
-        (subscriptionId: string)
+        (fileRepo: FileRepository)
+        (readArgs: FileReadOperationArguments)
+        (subscription: string)
         (path: string)
+        (failOnError: bool)
         (additionActions: string list)
         =
-        let metadata = [ "subscription_id", subscriptionId ] |> Map.ofList
-
-        let cfg = ConfigurationStore.Initialize(path, additionActions, metadata)
-
-        // Get pipelines
-        // Set
-
-
-        ()
+        Fetch.subscriptionByReference ctx subscription
+        |> FetchResult.toActionResult
+        |> ActionResult.bind (fun sub ->
+            let cfg = ConfigurationStore.Load(path)
+            
+            let cfgSubscriptionId = cfg.GetMetadataItem "subscription_id" |> Option.map (fun sid -> sid.ItemValue)
+            
+            // NOTE Should this default to 0?
+            let cfgTip = cfg.GetMetadataItem "serial_tip"
+                         |> Option.bind (fun st -> match Int32.TryParse st.ItemValue with | true, v -> Some v | false, _ -> None)
+                         |> Option.defaultValue 0
+            
+            match cfgSubscriptionId with
+            | Some sid when sid = sub.Reference ->
+                let events = Events.selectEventRecords ctx sub.Id cfgTip
+                
+                
+                // Set the new serial tip
+                cfg.AddMetadataItem("serial_tip", string 0, true)
+                
+                ActionResult.Success ()
+            | Some sid ->
+                ({ Message = $"Configuration store subscription (`{sid}`) does not match requested subscription `{sub.Reference}`"
+                   DisplayMessage = "Subscription mismatch"
+                   Exception = None }: FailureResult)
+                |> ActionResult.Failure
+            | None ->
+                ({ Message = $"Configuration store subscription has no subscription id set"
+                   DisplayMessage = "Subscription missing"
+                   Exception = None }: FailureResult)
+                |> ActionResult.Failure)
