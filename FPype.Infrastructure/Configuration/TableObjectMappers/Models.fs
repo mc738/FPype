5203e387a055dec402261414d4196f244911a9b7
Max Clifford <me@mclifford.dev>
2023-06-12 19:41:52 +0100

    Added table object mapper read ops

--- FPype.Infrastructure/Configuration/TableObjectMappers/Models.fs
+++ FPype.Infrastructure/Configuration/TableObjectMappers/Models.fs
@@ -23,3 +23,12 @@ module Models =
           MapperData: string
           Hash: string
           CreatedOn: DateTime }
+        
+    
+    type TableObjectMapperVersionOverview =
+        { MapperReference: string
+          Reference: string
+          Name: string
+          Version: int }
+
+    type TableObjectMapperOverview = { Reference: string; Name: string }
--- FPype.Infrastructure/Configuration/TableObjectMappers/ReadOperations.fs
+++ FPype.Infrastructure/Configuration/TableObjectMappers/ReadOperations.fs
@@ -80,3 +80,50 @@ module ReadOperations =
                    CreatedOn = mvr.CreatedOn } }
             : TableObjectMapperDetails))
         |> FetchResult.fromResult
+
+    let tableObjectMapperVersions (ctx: MySqlContext) (logger: ILogger) (userReference: string) (mapperReference: string) =
+        Fetch.user ctx userReference
+        |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
+        |> FetchResult.chain (fun (ur, sr) mr -> ur, sr, mr) (Fetch.tableObjectMapper ctx mapperReference)
+        |> FetchResult.merge (fun (ur, sr, mr) mvrs -> ur, sr, mr, mvrs) (fun (_, _, mr) ->
+            Fetch.tableVersionsByTableId ctx mr.Id)
+        |> FetchResult.toResult
+        // Verify
+        |> Result.bind (fun (ur, sr, mr, mvrs) ->
+            let verifiers =
+                [ Verification.userIsActive ur
+                  Verification.subscriptionIsActive sr
+                  Verification.userSubscriptionMatches ur mr.SubscriptionId ]
+
+            VerificationResult.verify verifiers (ur, sr, mr, mvrs))
+        // Map
+        |> Result.map (fun (ur, sr, mr, mvrs) ->
+            mvrs
+            |> List.map (fun mvr ->
+                ({ MapperReference = mr.Reference
+                   Reference = mvr.Reference
+                   Name = mr.Name
+                   Version = mvr.Version }
+                : TableObjectMapperVersionOverview)))
+        |> FetchResult.fromResult
+    
+    let tableObjectMappers (ctx: MySqlContext) (logger: ILogger) (userReference: string) =
+        Fetch.user ctx userReference
+        |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
+        |> FetchResult.merge (fun (ur, sr) prs -> ur, sr, prs) (fun (_, sr) ->
+            Fetch.tableObjectMappersBySubscriptionId ctx sr.Id)
+        |> FetchResult.toResult
+        // Verify
+        |> Result.bind (fun (ur, sr, mrs) ->
+            let verifiers =
+                [ Verification.userIsActive ur; Verification.subscriptionIsActive sr ]
+
+            VerificationResult.verify verifiers (ur, sr, mrs))
+        // Map
+        |> Result.map (fun (ur, sr, trs) ->
+            trs
+            |> List.map (fun pr ->
+                ({ Reference = pr.Reference
+                   Name = pr.Name }
+                : TableObjectMapperOverview)))
+        |> FetchResult.fromResult
