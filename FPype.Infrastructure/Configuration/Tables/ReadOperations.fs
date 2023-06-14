af585ba2371ac143246dc538e63a83874094d720
Max Clifford <me@mclifford.dev>
2023-06-14 19:08:50 +0100

    Refactored table read ops

--- FPype.Infrastructure/Configuration/Tables/ReadOperations.fs
+++ FPype.Infrastructure/Configuration/Tables/ReadOperations.fs
@@ -27,7 +27,7 @@ module ReadOperations =
                                Reference = tvr.Reference
                                Name = tr.Name
                                Version = tvr.Version
-                               CreatedOn = tvr.CreatedOn 
+                               CreatedOn = tvr.CreatedOn
                                Columns =
                                  tcs
                                  |> List.map (fun c ->
@@ -41,6 +41,23 @@ module ReadOperations =
                             : TableVersionDetails)
                             |> Some
                         | FetchResult.Failure fr -> None)))
+
+        let createTableColumnDetails (defaultBaseType: BaseType option) (column: Records.TableColumn) =
+            ({ Reference = column.Reference
+               Name = column.Name
+               Type =
+                 // If a default base type if provided that will be used in the (unlikely) event the base type is unknown.
+                 // If none is provided and error will be thrown.
+                 // However, this should be a rare occurrence.
+                 BaseType.FromId(column.DataType, column.Optional)
+                 |> Option.defaultWith (fun _ ->
+                     match defaultBaseType with
+                     | Some dbt -> dbt
+                     | None -> failwith $"Unknown base type: `{column.DataType}`")
+               Optional = column.Optional
+               ImportHandlerData = column.ImportHandlerJson
+               Index = column.ColumnIndex }
+            : TableColumnDetails)
 
     let latestTableVersion (ctx: MySqlContext) (logger: ILogger) (userReference: string) (tableReference: string) =
         Fetch.user ctx userReference
@@ -66,23 +83,20 @@ module ReadOperations =
                    Reference = tvr.Reference
                    Name = tr.Name
                    Version = tvr.Version
-                   CreatedOn = tvr.CreatedOn  
-                   Columns =
-                     tcs
-                     |> List.map (fun c ->
-                         ({ Reference = c.Reference
-                            Name = c.Name
-                            Type = BaseType.String // TODO sort out
-                            Optional = c.Optional
-                            ImportHandlerData = c.ImportHandlerJson
-                            Index = c.ColumnIndex }
-                         : TableColumnDetails)) }
+                   CreatedOn = tvr.CreatedOn
+                   Columns = tcs |> List.map (Internal.createTableColumnDetails (Some BaseType.String)) }
                 : TableVersionDetails)
                 |> Some
             | FetchResult.Failure fr -> None)
         |> optionalToFetchResult "Latest table version"
 
-    let specificTableVersion (ctx: MySqlContext) (logger: ILogger) (userReference: string) (tableReference: string) (version: int) =
+    let specificTableVersion
+        (ctx: MySqlContext)
+        (logger: ILogger)
+        (userReference: string)
+        (tableReference: string)
+        (version: int)
+        =
         Fetch.user ctx userReference
         |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
         |> FetchResult.chain (fun (ur, sr) tr -> ur, sr, tr) (Fetch.table ctx tableReference)
@@ -106,17 +120,8 @@ module ReadOperations =
                    Reference = tvr.Reference
                    Name = tr.Name
                    Version = tvr.Version
-                   CreatedOn = tvr.CreatedOn 
-                   Columns =
-                     tcs
-                     |> List.map (fun c ->
-                         ({ Reference = c.Reference
-                            Name = c.Name
-                            Type = BaseType.String // TODO sort out
-                            Optional = c.Optional
-                            ImportHandlerData = c.ImportHandlerJson
-                            Index = c.ColumnIndex }
-                         : TableColumnDetails)) }
+                   CreatedOn = tvr.CreatedOn
+                   Columns = tcs |> List.map (Internal.createTableColumnDetails (Some BaseType.String)) }
                 : TableVersionDetails)
                 |> Some
             | FetchResult.Failure fr -> None)
@@ -147,7 +152,7 @@ module ReadOperations =
                    Version = tvr.Version }
                 : TableVersionOverview)))
         |> FetchResult.fromResult
-    
+
     let table (ctx: MySqlContext) (logger: ILogger) (userReference: string) (tableReference: string) =
         Fetch.user ctx userReference
         |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
@@ -167,18 +172,26 @@ module ReadOperations =
         |> Result.map (fun (ur, sr, tr, tvrs) ->
             tvrs
             |> List.map (fun tvr ->
+                let columns =
+                    match Fetch.tableColumns ctx tvr.Id with
+                    | FetchResult.Success cs -> cs
+                    | FetchResult.Failure f ->
+                        // TODO log error
+                        []
+
                 ({ TableReference = tr.Reference
                    Reference = tvr.Reference
                    Name = tr.Name
-                   Version = tvr.Version }
-                : TableVersionOverview)))
+                   Version = tvr.Version
+                   CreatedOn = tvr.CreatedOn
+                   Columns = columns |> List.map (Internal.createTableColumnDetails (Some BaseType.String)) }
+                : TableVersionDetails)))
         |> FetchResult.fromResult
-    
+
     let tables (ctx: MySqlContext) (logger: ILogger) (userReference: string) =
         Fetch.user ctx userReference
         |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
-        |> FetchResult.merge (fun (ur, sr) trs -> ur, sr, trs) (fun (_, sr) ->
-            Fetch.tablesBySubscriptionId ctx sr.Id)
+        |> FetchResult.merge (fun (ur, sr) trs -> ur, sr, trs) (fun (_, sr) -> Fetch.tablesBySubscriptionId ctx sr.Id)
         |> FetchResult.toResult
         // Verify
         |> Result.bind (fun (ur, sr, trs) ->
