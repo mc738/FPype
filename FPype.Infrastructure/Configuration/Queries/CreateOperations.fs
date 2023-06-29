7a1428e35d29766f47553a9d7d9522bd59048780
Max Clifford <me@mclifford.dev>
2023-06-29 19:36:34 +0100

    Added serializedQueryVersion to CreateOperations.fs

--- FPype.Infrastructure/Configuration/Queries/CreateOperations.fs
+++ FPype.Infrastructure/Configuration/Queries/CreateOperations.fs
@@ -168,3 +168,55 @@ module CreateOperations =
                   |> Events.QueryVersionAdded ]
                 |> Events.addEvents t logger sr.Id ur.Id timestamp))
         |> toActionResult "Create raw query version"
+        
+    let serializedQueryVersion
+            (ctx: MySqlContext)
+            (logger: ILogger)
+            (userReference: string)
+            (queryReference: string)
+            (version: NewSerializedQueryVersion)
+            =
+            ctx.ExecuteInTransaction(fun t ->
+                // Fetch
+                Fetch.user t userReference
+                |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.Id)
+                |> FetchResult.chain (fun (ur, sr) qr -> ur, sr, qr) (Fetch.query t queryReference)
+                |> FetchResult.merge (fun (ur, sr, qr) qvr -> ur, sr, qr, qvr) (fun (_, _, qr) ->
+                    Fetch.queryLatestVersion t qr.Id)
+                |> FetchResult.toResult
+                // Verify
+                |> Result.bind (fun (ur, sr, qr, qvr) ->
+                    let verifiers =
+                        [ Verification.userIsActive ur
+                          Verification.subscriptionIsActive sr
+                          Verification.userSubscriptionMatches ur qr.SubscriptionId ]
+    
+                    VerificationResult.verify verifiers (ur, sr, qr, qvr))
+                // Create
+                |> Result.map (fun (ur, sr, qr, qvr) ->
+                    let timestamp = getTimestamp ()
+                    let serializedQuery = version.SerializedQuery.Serialize()
+                    let hash = serializedQuery.GetSHA256Hash()
+                    let versionNumber = qvr.Version + 1
+    
+                    ({ Reference = version.Reference
+                       QueryId = qr.Id
+                       Version = versionNumber
+                       RawQuery = serializedQuery
+                       Hash = hash
+                       IsSerialized = false 
+                       CreatedOn = timestamp }
+                    : Parameters.NewQueryVersion)
+                    |> Operations.insertQueryVersion t
+                    |> ignore
+    
+                    [ ({ Reference = version.Reference
+                         QueryReference = qr.Reference
+                         Version = versionNumber
+                         Hash = hash
+                         IsSerialized = true
+                         CreatedOnDateTime = timestamp }
+                      : Events.QueryVersionAddedEvent)
+                      |> Events.QueryVersionAdded ]
+                    |> Events.addEvents t logger sr.Id ur.Id timestamp))
+            |> toActionResult "Create serialized query version"
