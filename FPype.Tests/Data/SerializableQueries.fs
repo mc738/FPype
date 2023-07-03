56428cda838d6ae7e64b36b4df3bd9e3c849e4a1
Max Clifford <me@mclifford.dev>
2023-07-03 19:30:56 +0100

    Added "Convert less than or equals condition to and from json" test

--- FPype.Tests/Data/SerializableQueries.fs
+++ FPype.Tests/Data/SerializableQueries.fs
@@ -274,3 +274,18 @@ type SerializableQueryTests() =
             writeToJson condition.WriteToJson |> loadJson |> SerializableQueries.Condition.FromJson
             
         Assert.AreEqual(expected, actual)
+        
+    
+    [<TestMethod>]
+    member _.``Convert less than or equals condition to and from json``() =
+        let condition =
+            SerializableQueries.Condition.LessThanOrEquals(
+                SerializableQueries.Value.Field { TableName = "test_table"; Field = "foo" },
+                SerializableQueries.Value.Boolean true)
+        
+        let expected: Result<SerializableQueries.Condition, string> = Ok condition
+        
+        let actual =
+            writeToJson condition.WriteToJson |> loadJson |> SerializableQueries.Condition.FromJson
+            
+        Assert.AreEqual(expected, actual)
