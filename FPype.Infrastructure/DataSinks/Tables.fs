9bd72fe9caaa790a6f61e0f7a7c8d6c2c2a02bd2
Max Clifford <me@mclifford.dev>
2023-07-13 20:05:42 +0100

    More work on DataSinks.Tables

--- FPype.Infrastructure/DataSinks/Tables.fs
+++ FPype.Infrastructure/DataSinks/Tables.fs
@@ -1,55 +1,57 @@
 ﻿namespace FPype.Infrastructure.DataSinks
 
-open FPype.Core.Types
-open Microsoft.VisualBasic.CompilerServices
+open FsToolbox.Core.Results
+
 
 
 [<RequireQualifiedAccess>]
 module Tables =
 
-    open System.IO
-    open Freql.Sqlite    
-    open FPype.Data.Models
-    open FPype.Data.ModelExtensions
-    
-    let dataSinkColumns = [
-        ({ Name = "ds__id"
-           Type = BaseType.String
-           ImportHandler = None })
-        ({ Name = "ds__timestamp"
-           Type = BaseType.DateTime
-           ImportHandler = None })
-    ]
-    
-    let appendDataSinkColumns (table: TableModel) =
-        table.AppendColumns dataSinkColumns
-    
-    let appendDataSinkData (row: TableRow) =
-        ()
-    
-    
-    let initialize (path: string) (model: TableModel)  =
-        let fullPath = Path.Combine(path, ())
-        
-        match File.Exists path with
-        | true -> Ok ()
-        | false ->
-            
-            use ctx = SqliteContext.Create("")
-            model.AppendColumns(
-                [
-                    
-                ])
-            |> fun tm -> tm.
-            
-            
-            
-            Ok ()
-    
-    
-    
-    let insertRow (row: TableRow) =
-        
-        
-        ()
 
+    open System.IO
+    open Freql.Sqlite
+    open FPype.Core.Types
+    open FPype.Data.Models
+    open FPype.Data.ModelExtensions.Sqlite
+
+    let dataSinkColumns =
+        [ ({ Name = "ds__id"
+             Type = BaseType.String
+             ImportHandler = None })
+          ({ Name = "ds__timestamp"
+             Type = BaseType.DateTime
+             ImportHandler = None }) ]
+
+    let appendDataSinkColumns (table: TableModel) = table.AppendColumns dataSinkColumns
+
+    let appendDataSinkData (row: TableRow) = ()
+
+    let createTable (ctx: SqliteContext) (table: TableModel) = table.SqliteCreateTable ctx
+
+    let initialize (id: string) (path: string) (schema: TableSchema) =
+        let fullPath = Path.Combine(path, $"{id}.db")
+
+        match File.Exists fullPath with
+        | true -> Ok()
+        | false ->
+
+            use ctx = SqliteContext.Create("")
+
+            try
+                TableModel.FromSchema schema
+                |> appendDataSinkColumns
+                |> createTable ctx
+                |> ignore
+                |> Ok
+            with exn ->
+                ({ Message = $"Error creating `({schema.Name})` table: {exn.Message}"
+                   DisplayMessage = $"Error creating `({schema.Name})` table"
+                   Exception = Some exn }
+                : FailureResult)
+                |> Error
+        |> ActionResult.fromResult
+
+    let insertRow (row: TableRow) =
+
+
+        ()
