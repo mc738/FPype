40631682c5dce6930e5dd3aee64aa3b496b6b433
Max Clifford <me@mclifford.dev>
2023-07-20 19:36:09 +0100

    Fix MySql errors 4

--- FPype/Connectors/MySql.fs
+++ FPype/Connectors/MySql.fs
@@ -7,52 +7,20 @@ module MySql =
     open FPype.Core.Types
     open FPype.Data.Models
     open MySql.Data.MySqlClient
-
-    [<AutoOpen>]
-    module private Internal =
-
-        let mapper (columns: TableColumn list) (reader: MySqlDataReader) =
-            let rec handler t (i: int) =
-                // NOTE This uses the int override of Get[type] because IsDBNull requires an int.
-                match t with
-                | BaseType.Boolean -> reader.GetBoolean i |> Value.Boolean
-                | BaseType.Byte -> reader.GetByte i |> Value.Byte
-                | BaseType.Char -> reader.GetChar i |> Value.Char
-                | BaseType.Decimal -> reader.GetDecimal i |> Value.Decimal
-                | BaseType.Double -> reader.GetDouble i |> Value.Double
-                | BaseType.Float -> reader.GetFloat i |> Value.Float
-                | BaseType.Int -> reader.GetInt32 i |> Value.Int
-                | BaseType.Short -> reader.GetInt16 i |> Value.Short
-                | BaseType.Long -> reader.GetInt64 i |> Value.Long
-                | BaseType.String -> reader.GetString i |> Value.String
-                | BaseType.DateTime -> reader.GetDateTime i |> Value.DateTime
-                | BaseType.Guid -> reader.GetGuid i |> Value.Guid
-                | BaseType.Option it ->
-                    match reader.IsDBNull i with
-                    | true -> None |> Value.Option
-                    | false -> handler it i |> Some |> Value.Option
-
-            [ while reader.Read() do
-                  columns
-                  |> List.map (fun c ->
-                      let i = reader.GetOrdinal c.Name
-                      handler c.Type i)
-                  |> TableRow.FromValues ]
+    open FPype.Data.ModelExtensions.MySql
 
     let selectConditional (connectionString: string) (table: TableModel) (conditions: string) (parameters: obj list) =
         use ctx = MySqlContext.Connect(connectionString)
 
-        table.SqliteConditionalSelect(ctx, conditions, parameters)
+        table.MySqlConditionalSelect(ctx, conditions, parameters)
 
-    let selectBespoke (path: string) (table: TableModel) (sql: string) (parameters: obj list) =
-        use ctx = SqliteContext.Open(path)
+    let selectBespoke (connectionString: string) (table: TableModel) (sql: string) (parameters: obj list) =
+        use ctx = MySqlContext.Connect(connectionString)
 
-        table.SqliteBespokeSelect(ctx, sql, parameters)
+        table.MySqlBespokeSelect(ctx, sql, parameters)
 
-    let insert (path: string) (table: TableModel) =
-        use ctx = SqliteContext.Open(path)
-
-        table.SqliteInsert(ctx)
+    let insert (connectionString: string) (table: TableModel) =
+        use ctx = MySqlContext.Connect(connectionString)
 
 
-    ()
+        table.MySqlInsert(ctx)
