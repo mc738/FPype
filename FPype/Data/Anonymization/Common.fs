bc7732f9d374b933ea888f319f9ce2157918c7c2
Max Clifford <me@mclifford.dev>
2023-07-26 19:23:29 +0100

    Added AnonymizationContext

--- FPype/Data/Anonymization/Common.fs
+++ FPype/Data/Anonymization/Common.fs
@@ -1,7 +1,19 @@
 ﻿namespace FPype.Data.Anonymization
 
+open System
+
 module Common =
     
+    type AnonymizationContext =
+        { RNG: Random }
+
+        static member Create(seed) = { RNG = Random(seed) }
+
+        member ctx.NextRandom() = ctx.RNG.Next()
+
+        member ctx.NextRandom(min, max) = ctx.RNG.Next(min, max)
+
+        member ctx.NextChance(value: float) = ctx.RNG.NextDouble() <= value
     
     
     ()
