1ced27425a02ddd3432d7a1cea0666adcec68ce4
Max Clifford <me@mclifford.dev>
2023-07-27 18:01:20 +0100

    Added ValidationStep

--- FPype/Data/Cleansing/Common.fs
+++ FPype/Data/Cleansing/Common.fs
@@ -3,21 +3,29 @@
 [<AutoOpen>]
 module Common =
     
-    type CleansingAction =
-        | Transform
-        | Validate
-        
-    and TransformationStep =
-        | RemoveCharacters of Characters : char list
-        | StringReplace of Value : string * Replacement : string
-        | Bespoke of Handler: (string -> string)
-    
-    
     [<RequireQualifiedAccess>]
     type CleansingResult =
         | Untouched of string
         | Modified of string
         | Failure of Message: string
     
-    ()
-
+    type [<RequireQualifiedAccess>] CleansingAction =
+        | Transform of Step: TransformationStep
+        | Validate of Step: ValidationStep
+        
+    
+    and [<RequireQualifiedAccess>] TransformationStep =
+        | RemoveCharacters of Characters : char list
+        | StringReplace of Value : string * Replacement : string
+        | RegexReplace of Pattern : string * Replacement : string
+        | Bespoke of Handler: (string -> string)
+    
+    and [<RequireQualifiedAccess>] ValidationStep =
+        | ContainsCharacters of Characters: char list
+        | Contains
+        | RegexMatch of Pattern : string
+        | Not of Step: ValidationStep
+        | AnyOf of Steps: ValidationStep option
+        | AllOf of Steps: ValidationStep option
+        | Bespoke of Handler: (string -> bool)
+    
