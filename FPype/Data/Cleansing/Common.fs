80115a5270c1db1d26db213f92cba9379a209123
Max Clifford <me@mclifford.dev>
2023-07-27 17:54:28 +0100

    Started work on data cleansing

---
+++ FPype/Data/Cleansing/Common.fs
@@ -0,0 +1,23 @@
+﻿namespace FPype.Data.Cleansing
+
+[<AutoOpen>]
+module Common =
+    
+    type CleansingAction =
+        | Transform
+        | Validate
+        
+    and TransformationStep =
+        | RemoveCharacters of Characters : char list
+        | StringReplace of Value : string * Replacement : string
+        | Bespoke of Handler: (string -> string)
+    
+    
+    [<RequireQualifiedAccess>]
+    type CleansingResult =
+        | Untouched of string
+        | Modified of string
+        | Failure of Message: string
+    
+    ()
+
