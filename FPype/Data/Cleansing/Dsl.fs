63e88fa4477afd6c9d7a1a6b12a7986bea80a0c7
Max Clifford <me@mclifford.dev>
2023-07-29 12:31:41 +0100

    Work on cleansing dsl

--- FPype/Data/Cleansing/Dsl.fs
+++ FPype/Data/Cleansing/Dsl.fs
@@ -1,2 +1,13 @@
-﻿module FPype.Data.Cleansing.Dsl
+﻿module FPype.Data.Cleansing
 
+open FPype.Data.Cleansing
+
+module Dsl =
+
+    let validate (step: ValidationStep) = CleansingStep.Validate step
+
+    let validation (steps: ValidationStep list) = steps |> List.map validate
+
+    let transform (step: TransformationStep) = CleansingStep.Transform step
+
+    let transformation (steps: TransformationStep list) = steps |> List.map transform
