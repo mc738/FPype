f7280058814a1ff07807eefa2f93a4428754e2a3
Max Clifford <me@mclifford.dev>
2023-07-29 12:39:23 +0100

    Work on cleansing dsl 2

--- FPype/Data/Cleansing/Dsl.fs
+++ FPype/Data/Cleansing/Dsl.fs
@@ -11,13 +11,24 @@ module Dsl =
     let transform (step: TransformationStep) = CleansingStep.Transform step
 
     let transformation (steps: TransformationStep list) = steps |> List.map transform
-    
-    let containsCharacters (chars: char list) = ValidationStep.ContainsCharacters chars |> validate
-    
+
+    let containsCharacters (chars: char list) =
+        ValidationStep.ContainsCharacters chars |> validate
+
+    let doesNotContainCharacters (chars: char list) =
+        ValidationStep.ContainsCharacters chars |> ValidationStep.Not |> validate
+
     let contains (str: string) = ValidationStep.Contains str |> validate
-    
+
+    let doesNotContain (str: string) =
+        ValidationStep.Contains str |> ValidationStep.Not |> validate
+
     let containsLetters = ValidationStep.ContainsLetters |> validate
-    
+
+    let doesNotContainLetters =
+        ValidationStep.ContainsLetters |> ValidationStep.Not |> validate
+
     let containsNumbers = ValidationStep.ContainsNumbers |> validate
-    
-    
+
+    let doesNotContainNumbers =
+        ValidationStep.ContainsNumbers |> ValidationStep.Not |> validate
