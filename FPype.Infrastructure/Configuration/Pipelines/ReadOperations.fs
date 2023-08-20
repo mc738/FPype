a38b19a2614ae7cac3d48404ebb712d985932335
Max Clifford <me@mclifford.dev>
2023-08-20 11:20:20 +0100

    Fix ups 3

--- FPype.Infrastructure/Configuration/Pipelines/ReadOperations.fs
+++ FPype.Infrastructure/Configuration/Pipelines/ReadOperations.fs
@@ -45,6 +45,7 @@ module ReadOperations =
                    Name = pr.Name
                    Description = pvr.Description
                    Version = pvr.Version
+                   CreatedOn = pvr.CreatedOn 
                    Actions =
                      ars
                      |> List.choose (fun ar ->
@@ -97,6 +98,7 @@ module ReadOperations =
                    Name = pr.Name
                    Description = pvr.Description
                    Version = pvr.Version
+                   CreatedOn = pvr.CreatedOn 
                    Actions =
                      ars
                      |> List.choose (fun ar ->
@@ -160,4 +162,60 @@ module ReadOperations =
                 ({ Reference = pr.Reference
                    Name = pr.Name }
                 : PipelineOverview)))
+        |> FetchResult.fromResult
+
+    let pipeline (ctx: MySqlContext) (logger: ILogger) (userReference: string) (pipelineReference: string) =
+        Fetch.user ctx userReference
+        |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
+        |> FetchResult.chain (fun (ur, sr) pr -> ur, sr, pr) (Fetch.pipeline ctx pipelineReference)
+        |> FetchResult.merge (fun (ur, sr, pr) pvrs -> ur, sr, pr, pvrs) (fun (_, _, pr) ->
+            Fetch.pipelineVersionsByPipelineId ctx pr.Id)
+        |> FetchResult.toResult
+        // Verify
+        |> Result.bind (fun (ur, sr, pr, pvrs) ->
+            let verifiers =
+                [ Verification.userIsActive ur
+                  Verification.subscriptionIsActive sr
+                  Verification.subscriptionMatches sr pr.SubscriptionId ]
+
+            VerificationResult.verify verifiers (ur, sr, pr, pvrs))
+        // Map
+        |> Result.map (fun (ur, sr, pr, pvrs) ->
+            let typeMap =
+                Operations.selectActionTypeRecords ctx [] []
+                |> List.map (fun atr -> atr.Id, atr.Name)
+                |> Map.ofList
+                
+            // TODO what to do if this fails?
+            let versions =
+                pvrs
+                |> List.choose (fun pvr ->
+                    match Fetch.pipelineActions ctx pvr.Id with
+                    | FetchResult.Success ars ->
+                        ({ PipelineReference = pr.Reference
+                           VersionReference = pvr.Reference
+                           Name = pr.Name
+                           Description = pvr.Description
+                           Version = pvr.Version
+                           CreatedOn = pvr.CreatedOn 
+                           Actions =
+                             ars
+                             |> List.choose (fun ar ->
+                                 typeMap.TryFind ar.ActionTypeId
+                                 |> Option.map (fun atn ->
+                                     ({ Reference = ar.Reference
+                                        Name = ar.Name
+                                        ActionType = atn
+                                        ActionData = ar.ActionJson
+                                        Hash = ar.Hash
+                                        Step = ar.Step }
+                                     : PipelineActionDetails))) }
+                        : PipelineVersionDetails)
+                        |> Some
+                    | FetchResult.Failure fr -> None)
+            
+            ({ Reference = pr.Reference
+               Name = pr.Name
+               Versions = versions }
+            : PipelineDetails))
         |> FetchResult.fromResult
