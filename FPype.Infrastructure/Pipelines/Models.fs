22398b8f8b7036eb76c770eb4b8b8b8c3e2c05a7
Max Clifford <me@mclifford.dev>
2023-08-21 19:20:07 +0100

    Added Pipeline models

---
+++ FPype.Infrastructure/Pipelines/Models.fs
@@ -0,0 +1,23 @@
+﻿namespace FPype.Infrastructure.Pipelines
+
+open System
+
+module Models =
+    
+    type PipelineRunDetails =
+        {
+            RunId: string
+            SubscriptionId: string
+            PipelineReference: string
+            PipelineName: string
+            PipelineVersion: int
+            PipelineVersionReference: string
+            QueuedOn: DateTime 
+            StartedOn: DateTime option
+            CompletedOn: DateTime option
+            WasSuccessful: bool option
+            BasePath: string
+            RunByReference: string
+            RunByName: string
+        }
+
