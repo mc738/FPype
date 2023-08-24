4a8a880e0d82a9902bd7df625b69ce5be19d24bb
Max Clifford <me@mclifford.dev>
2023-08-24 18:52:06 +0100

    Added getPipelineRunItemForUser

--- FPype.Infrastructure/Pipelines/Operations.fs
+++ FPype.Infrastructure/Pipelines/Operations.fs
@@ -1,6 +1,7 @@
 ﻿namespace FPype.Infrastructure.Pipelines
 
 open System
+open FPype.Infrastructure.Pipelines.Models
 open FsToolbox.Core.Results
 open Microsoft.Extensions.Logging
 
@@ -72,7 +73,7 @@ module Operations =
                RunByName = rur.Username }
             : Models.PipelineRunDetails))
         |> FetchResult.fromResult
-        
+
     /// <summary>
     /// A function to return a pipeline run item.
     /// This is meant for internal use and doesn't require a user reference like getPipelineRunItem.
@@ -81,18 +82,14 @@ module Operations =
     /// <param name="ctx">The MySql database context.</param>
     /// <param name="logger">The logger.</param>
     /// <param name="runId">The run id.</param>
-    let getPipelineRunItemInternal
-        (ctx: MySqlContext)
-        (logger: ILogger)
-        (runId: string)
-        =
+    let getPipelineRunItemInternal (ctx: MySqlContext) (logger: ILogger) (runId: string) =
         Internal.fetchPipelineRunItem ctx runId
         |> FetchResult.merge (fun pir sr -> sr, pir) (fun pir -> Fetch.subscriptionById ctx pir.SubscriptionId)
-        |> FetchResult.merge (fun (sr, pri) pvr -> sr, pvr, pri) (fun ( _, pvr) ->
+        |> FetchResult.merge (fun (sr, pri) pvr -> sr, pvr, pri) (fun (_, pvr) ->
             Fetch.pipelineVersionById ctx pvr.PipelineVersionId)
         |> FetchResult.merge (fun (sr, pvr, pri) pr -> sr, pr, pvr, pri) (fun (_, pvr, _) ->
             Fetch.pipelineById ctx pvr.PipelineId)
-        |> FetchResult.merge (fun (sr, pr, pvr, pir) rur -> sr, pr, pvr, pir, rur) (fun ( _, _, _, pir) ->
+        |> FetchResult.merge (fun (sr, pr, pvr, pir) rur -> sr, pr, pvr, pir, rur) (fun (_, _, _, pir) ->
             Fetch.userById ctx pir.RunBy)
         |> FetchResult.toResult
         // Verify
@@ -117,6 +114,44 @@ module Operations =
                RunByReference = rur.Reference
                RunByName = rur.Username }
             : Models.PipelineRunDetails))
+        |> FetchResult.fromResult
+
+    let getPipelineRunItemsForUser (ctx: MySqlContext) (logger: ILogger) (userReference: string) =
+        Fetch.user ctx userReference
+        |> FetchResult.merge (fun ur sr -> sr, ur) (fun ur -> Fetch.subscriptionById ctx ur.SubscriptionId)
+        |> FetchResult.toResult
+        // Verify
+        |> Result.bind (fun (sr, ur) ->
+            let verifiers =
+                [ Verification.subscriptionIsActive sr; Verification.userIsActive ur ]
+
+            VerificationResult.verify verifiers (sr, ur))
+        |> Result.map (fun (sr, ur) ->
+            let sql =
+                """
+            SELECT 
+                pr.reference AS run_id,
+                s.reference AS subscription_reference,
+                cp.reference AS pipeline_reference,
+                cp.name AS pipeline_name,
+                cpv.reference AS pipeline_version_reference,
+                cpv.version AS pipeline_version,
+                pr.queued_on AS queued_on,
+                pr.started_on AS started_on,
+                pr.completed_on AS completed_on,
+                pr.was_successful AS was_successful,
+                pr.base_path AS base_path,
+                u.reference AS run_by_reference,
+                u.username AS run_by_name
+            FROM pipeline_runs pr 
+            JOIN subscriptions s ON pr.subscription_id = s.id
+            JOIN cfg_pipeline_versions cpv ON pr.pipeline_version_id = cpv.id 
+            JOIN cfg_pipelines cp ON cpv.pipeline_id = cp.id
+            JOIN users u ON pr.run_by = u.id
+            WHERE s.reference = @0;
+            """
+
+            ctx.SelectAnon<PipelineRunDetails>(sql, [ sr.Reference ]))
         |> FetchResult.fromResult
 
     let queuePipelineRun
