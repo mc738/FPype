152c33c3b64d8f9a5b9ad50a44ad8748cd07ed2d
Max Clifford <me@mclifford.dev>
2023-08-25 18:59:09 +0100

    Added GetSubscriptionPipelines()

--- FPype.Infrastructure/Services/ConfigurationService.fs
+++ FPype.Infrastructure/Services/ConfigurationService.fs
@@ -24,16 +24,19 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.GetSpecificPipelineVersion(userReference, pipelineReference, version) =
         Configuration.Pipelines.ReadOperations.specificPipelineVersion ctx log userReference pipelineReference version
-    
+
     member _.GetPipelines(userReference) =
         Configuration.Pipelines.ReadOperations.pipelines ctx log userReference
-    
+
     member _.GetPipeline(userReference, pipelineReference) =
         Configuration.Pipelines.ReadOperations.pipeline ctx log userReference pipelineReference
-        
+
     member _.GetPipelineVersions(userReference, pipelineReference) =
         Configuration.Pipelines.ReadOperations.pipelineVersions ctx log userReference pipelineReference
-    
+
+    member _.GetSubscriptionPipelines(userReference) =
+        Configuration.Pipelines.ReadOperations.subscriptionPipelines ctx log userReference
+
     member _.AddTable(userReference, table) =
         Configuration.Tables.CreateOperations.table ctx log userReference table
 
@@ -51,22 +54,22 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.GetTable(userReference, tableReference) =
         Configuration.Tables.ReadOperations.table ctx log userReference tableReference
-        
+
     member _.GetTableVersions(userReference, tableReference) =
         Configuration.Tables.ReadOperations.tableVersions ctx log userReference tableReference
-    
+
     member _.AddRawQuery(userReference, query) =
         Configuration.Queries.CreateOperations.rawQuery ctx log userReference query
-    
+
     member _.AddSerializedQuery(userReference, query) =
         Configuration.Queries.CreateOperations.serializedQuery ctx log userReference query
-    
+
     member _.AddRawQueryVersion(userReference, queryReference, version) =
         Configuration.Queries.CreateOperations.rawQueryVersion ctx log userReference queryReference version
-    
+
     member _.AddSerializedQueryVersion(userReference, queryReference, version) =
         Configuration.Queries.CreateOperations.serializedQueryVersion ctx log userReference queryReference version
-    
+
     member _.GetLatestQueryVersion(userReference, queryReference) =
         Configuration.Queries.ReadOperations.latestQueryVersion ctx log userReference queryReference
 
@@ -75,13 +78,13 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.GetQueries(userReference) =
         Configuration.Queries.ReadOperations.queries ctx log userReference
-    
+
     member _.GetQuery(userReference, queryReference) =
         Configuration.Queries.ReadOperations.query ctx log userReference queryReference
-    
+
     member _.GetQueryVersions(userReference, queryReference) =
         Configuration.Queries.ReadOperations.queryVersions ctx log userReference queryReference
-    
+
     member _.AddResource(userReference, resource) =
         Configuration.Resources.CreateOperations.resource ctx log userReference resource
 
@@ -96,10 +99,10 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.GetResources(userReference) =
         Configuration.Resources.ReadOperations.resources ctx log userReference
-    
+
     member _.GetResourceVersions(userReference, resourceReference) =
         Configuration.Resources.ReadOperations.resourceVersions ctx log userReference resourceReference
-    
+
     member _.AddTableObjectMapper(userReference, mapper) =
         Configuration.TableObjectMappers.CreateOperations.tableObjectMapper ctx log userReference mapper
 
@@ -128,13 +131,13 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.AddObjectTableMapper(userReference, mapper) =
         Configuration.ObjectTableMappers.CreateOperations.objectTableMapper ctx log userReference mapper
-    
+
     member _.GetTableObjectMappers(userReference) =
         Configuration.TableObjectMappers.ReadOperations.tableObjectMappers ctx log userReference
-    
+
     member _.GetTableObjectMapperVersions(userReference, mapperReference) =
         Configuration.TableObjectMappers.ReadOperations.tableObjectMapperVersions ctx log userReference mapperReference
-    
+
     member _.AddObjectTableMapperVersion(userReference, mapperReference, version) =
         Configuration.ObjectTableMappers.CreateOperations.objectTableMapperVersion
             ctx
@@ -160,10 +163,9 @@ type ConfigurationService(ctx: MySqlContext, log: ILogger<ConfigurationService>)
 
     member _.GetObjectTableMappers(userReference) =
         Configuration.ObjectTableMappers.ReadOperations.objectTableMappers ctx log userReference
-    
+
     member _.GetObjectTableMapperVersions(userReference, mapperReference) =
         Configuration.ObjectTableMappers.ReadOperations.objectTableMapperVersions ctx log userReference mapperReference
-    
+
     member _.BuildConfigurationStore(fileRepo, readArgs, subscriptionId, path, failOnError, additionalActions) =
-       buildStore ctx log fileRepo readArgs subscriptionId path failOnError additionalActions
-       
+        buildStore ctx log fileRepo readArgs subscriptionId path failOnError additionalActions
