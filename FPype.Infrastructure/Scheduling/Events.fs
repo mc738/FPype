8759587c8f4e6f01e6594c3f83b93d886e8296f3
Max Clifford <me@mclifford.dev>
2023-09-02 13:19:44 +0100

    Added ScheduleCreatedEvent

--- FPype.Infrastructure/Scheduling/Events.fs
+++ FPype.Infrastructure/Scheduling/Events.fs
@@ -1,16 +1,24 @@
 ﻿namespace FPype.Infrastructure.Scheduling
 
+open System.Text.Json.Serialization
+
 [<RequireQualifiedAccess>]
 module Events =
-    
-    
+
+
     type ScheduleEvent =
-        | ScheduleCreated
+        | ScheduleCreated of ScheduleCreatedEvent
         | ScheduleUpdated
         | ScheduleActivated
         | ScheduleDeactivated
-        
+
+
+    and [<CLIMutable>] ScheduleCreatedEvent =
+        { [<JsonPropertyName("reference")>]
+          Reference: string
+          [<JsonPropertyName("scheduleCron")>]
+          ScheduleCron: string }
+
     
-    
-    
+
     ()
