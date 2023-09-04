c0b070b8fa722deda2ede5aa982091e5506ab002
Max Clifford <me@mclifford.dev>
2023-09-04 20:02:10 +0100

    Added Fetch.scheduleByReference

--- FPype.Infrastructure/Scheduling/Common.fs
+++ FPype.Infrastructure/Scheduling/Common.fs
@@ -27,3 +27,19 @@ module Common =
                   Exception = Some ex }
                 |> FetchResult.Failure
 
+        let scheduleByReference (ctx: MySqlContext) (reference: string) =
+            try
+                Operations.selectPipelineScheduleEventRecord ctx [ "WHERE reference = @0" ] [ reference ]
+                |> Option.map FetchResult.Success
+                |> Option.defaultWith (fun _ ->
+                    ({ Message = $"Schedule (ref: {reference}) not found"
+                       DisplayMessage = "Schedule not found"
+                       Exception = None }
+                    : FailureResult)
+                    |> FetchResult.Failure)
+            with ex ->
+                { Message = "Unhandled exception while fetching schedule"
+                  DisplayMessage = "Error fetching schedule"
+                  Exception = Some ex }
+                |> FetchResult.Failure
+
