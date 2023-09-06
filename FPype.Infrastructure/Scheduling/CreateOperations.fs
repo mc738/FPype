5bc8a27104d2eb3bcff8efea6f846f6d50ffe031
Max Clifford <me@mclifford.dev>
2023-09-06 22:01:35 +0100

    More work on schedule

--- FPype.Infrastructure/Scheduling/CreateOperations.fs
+++ FPype.Infrastructure/Scheduling/CreateOperations.fs
@@ -1,15 +1,17 @@
 ﻿namespace FPype.Infrastructure.Scheduling
 
-open FPype.Infrastructure.Scheduling.Models
-open Freql.MySql
-
 [<RequireQualifiedAccess>]
 module CreateOperations =
-    
+
+    open FPype.Infrastructure.Scheduling.Models
+    open Freql.MySql
+    open FPype.Infrastructure.Configuration.Common
+    open FsToolbox.Core.Results
+        
     let schedule (ctx: MySqlContext) (userReference: string) (schedule: NewSchedule) =
-        
-        
-        ()
-    
-    
-    ()
+        ctx.ExecuteInTransaction(fun t ->
+            Fetch.user t userReference
+            |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.Id)
+            
+            
+            )
