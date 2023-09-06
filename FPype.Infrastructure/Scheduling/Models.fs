3e91f2d76146e2936ffbcccf8b94fecc4c3b1077
Max Clifford <me@mclifford.dev>
2023-09-06 20:33:54 +0100

    Added ScheduleOverview

--- FPype.Infrastructure/Scheduling/Models.fs
+++ FPype.Infrastructure/Scheduling/Models.fs
@@ -5,3 +5,10 @@ module Models =
     type NewSchedule =
         { PipelineVersionReference: string
           ScheduleCron: string }
+
+
+    type ScheduleOverview =
+        { Reference: string
+          SubscriptionId: string
+          PipelineVersionReference: string
+          ScheduleCron: string }
