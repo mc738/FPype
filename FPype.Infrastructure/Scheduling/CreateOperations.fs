63f0544de7a5dcf23d9849fcff0b635c44c0ce8c
Max Clifford <me@mclifford.dev>
2023-09-07 18:54:26 +0100

    Added Verify to schedule

--- FPype.Infrastructure/Scheduling/CreateOperations.fs
+++ FPype.Infrastructure/Scheduling/CreateOperations.fs
@@ -1,5 +1,7 @@
 ﻿namespace FPype.Infrastructure.Scheduling
 
+open FPype.Infrastructure.Core
+
 [<RequireQualifiedAccess>]
 module CreateOperations =
 
@@ -14,4 +16,12 @@ module CreateOperations =
             |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.Id)
             |> FetchResult.chain (fun (ur, sr) pvr -> ur, sr, pvr) (Fetch.pipelineVersionByReference t schedule.PipelineVersionReference)
             |> FetchResult.merge (fun (ur, sr, pvr) pr -> ur, sr, pr, pvr) (fun (ur, sr, pvr) -> Fetch.pipelineById t pvr.PipelineId)
-            |> FetchResult.toResult)
+            |> FetchResult.toResult
+            // Verify
+            |> Result.bind (fun (ur, sr, pr, pvr) ->
+                let verifiers =
+                    [ Verification.userIsActive ur
+                      Verification.subscriptionIsActive sr
+                      Verification.userSubscriptionMatches ur pr.SubscriptionId ]
+
+                VerificationResult.verify verifiers (ur, sr, pr, pvr)))
