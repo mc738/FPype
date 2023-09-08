68acf6cdba5fa036a7f60abfca5b12d9f9933ebb
Max Clifford <me@mclifford.dev>
2023-09-08 19:58:04 +0100

    Started work on UpdateOperations.schedule

--- FPype.Infrastructure/Scheduling/UpdateOperations.fs
+++ FPype.Infrastructure/Scheduling/UpdateOperations.fs
@@ -3,5 +3,16 @@
 [<RequireQualifiedAccess>]
 module UpdateOperations =
     
-    ()
+    open Microsoft.Extensions.Logging
+    open FPype.Infrastructure.Scheduling.Models
+    open Freql.MySql
+    open FPype.Infrastructure.Configuration.Common
+    open FsToolbox.Core.Results
+    open FPype.Infrastructure.Core
+    open FPype.Infrastructure.Core.Persistence
+    
+    let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (schedule: UpdateSchedule) =
+        
+        ()
+    
 
