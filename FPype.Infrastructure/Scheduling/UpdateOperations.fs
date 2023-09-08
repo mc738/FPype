e117ec4986d5e495d4b6c3fb22a9c6cae7639d02
Max Clifford <me@mclifford.dev>
2023-09-08 19:59:52 +0100

    Work on UpdateOperations.fs

--- FPype.Infrastructure/Scheduling/UpdateOperations.fs
+++ FPype.Infrastructure/Scheduling/UpdateOperations.fs
@@ -11,8 +11,15 @@ module UpdateOperations =
     open FPype.Infrastructure.Core
     open FPype.Infrastructure.Core.Persistence
     
-    let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (schedule: UpdateSchedule) =
-        
-        ()
+    let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (update: UpdateSchedule) =
+        ctx.ExecuteInTransaction(fun t ->
+            // Fetch
+            Fetch.user t userReference
+            |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.Id)
+            |> FetchResult.chain (fun (ur, sr) pr -> ur, sr, pr) (Fetch.pipeline t pipelineReference)
+            |> FetchResult.merge (fun (ur, sr, pr) pvr -> ur, sr, pr, pvr) (fun (_, _, pr) ->
+                Fetch.pipelineLatestVersion t pr.Id)
+            |> FetchResult.toResult
+            )
     
 
