ceb026de2ab2173ba16dffac6a0985e71702d61e
Max Clifford <me@mclifford.dev>
2023-09-09 12:36:44 +0100

    Finised up update schedule

--- FPype.Infrastructure/Scheduling/UpdateOperations.fs
+++ FPype.Infrastructure/Scheduling/UpdateOperations.fs
@@ -1,8 +1,10 @@
 ﻿namespace FPype.Infrastructure.Scheduling
 
+open System.Security.Cryptography.Xml
+
 [<RequireQualifiedAccess>]
 module UpdateOperations =
-    
+
     open Microsoft.Extensions.Logging
     open FPype.Infrastructure.Scheduling.Models
     open Freql.MySql
@@ -10,15 +12,23 @@ module UpdateOperations =
     open FsToolbox.Core.Results
     open FPype.Infrastructure.Core
     open FPype.Infrastructure.Core.Persistence
-    
-    let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (scheduleReference: string) (update: UpdateSchedule) =
+
+    let schedule
+        (ctx: MySqlContext)
+        (logger: ILogger)
+        (userReference: string)
+        (scheduleReference: string)
+        (update: UpdateSchedule)
+        =
         ctx.ExecuteInTransaction(fun t ->
             // Fetch
             Fetch.user t userReference
             |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.Id)
             |> FetchResult.chain (fun (ur, sr) psr -> ur, sr, psr) (Fetch.scheduleByReference t scheduleReference)
-            |> FetchResult.merge (fun (ur, sr, psr) pvr -> ur, sr, pvr, psr) (fun (_, _, psr) -> Fetch.pipelineVersionById t psr.PipelineVersionId)
-            |> FetchResult.merge (fun (ur, sr, pvr, psr) pr -> ur, sr, pr, pvr, psr) (fun (_, _, pvr, _) -> Fetch.pipelineById t pvr.PipelineId)
+            |> FetchResult.merge (fun (ur, sr, psr) pvr -> ur, sr, pvr, psr) (fun (_, _, psr) ->
+                Fetch.pipelineVersionById t psr.PipelineVersionId)
+            |> FetchResult.merge (fun (ur, sr, pvr, psr) pr -> ur, sr, pr, pvr, psr) (fun (_, _, pvr, _) ->
+                Fetch.pipelineById t pvr.PipelineId)
             |> FetchResult.toResult
             // Verify
             |> Result.bind (fun (ur, sr, pr, pvr, psr) ->
@@ -28,8 +38,15 @@ module UpdateOperations =
                       Verification.userSubscriptionMatches ur pr.SubscriptionId
                       Verification.scheduleIsActive psr ]
 
-                VerificationResult.verify verifiers (ur, sr, pr, pvr))
-            
-            )
-    
+                VerificationResult.verify verifiers (ur, sr, pr, pvr, psr))
+            |> Result.map (fun (ur, sr, pr, pvr, psr) ->
+                t.ExecuteAnonNonQuery(
+                    "UPDATE pipeline_schedules SET schedule_cron = @0 WHERE id = @1",
+                    [ update.NewScheduleCron, psr.Id ]
+                )
+                |> ignore
 
+                [ Events.ScheduleEvent.ScheduleUpdated
+                      { Reference = psr.Reference
+                        NewScheduleCron = update.NewScheduleCron } ]
+                |> FPype.Infrastructure.Scheduling.Events.addEvents t logger sr.Id ur.Id (getTimestamp ())))
