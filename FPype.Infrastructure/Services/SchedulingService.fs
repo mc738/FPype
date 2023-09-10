8b843d76413b4e5eafd98865cdb918ec49400bf2
Max Clifford <me@mclifford.dev>
2023-09-10 21:12:37 +0100

    Fixed error

--- FPype.Infrastructure/Services/SchedulingService.fs
+++ FPype.Infrastructure/Services/SchedulingService.fs
@@ -13,7 +13,10 @@ type SchedulingService(ctx: MySqlContext, log: ILogger<SchedulingService>) =
         Scheduling.UpdateOperations.schedule ctx log userReference scheduleReference update
     
     member _.ActivateSchedule(userReference, scheduleReference) =
-        Scheduling.UpdateOperations.activateSchedule userReference scheduleReference
+        Scheduling.UpdateOperations.activateSchedule ctx log userReference scheduleReference
         
     member _.DeactivateSchedule(userReference, scheduleReference) =
-        Scheduling.UpdateOperations.deactivateSchedule userReference scheduleReference
+        Scheduling.UpdateOperations.deactivateSchedule ctx log userReference scheduleReference
+        
+    member _.GetSerialTipInternal() =
+        
