6ddedd7526f441232736f59625679eec17ca34b2
Max Clifford <me@mclifford.dev>
2023-09-10 21:16:29 +0100

    Added GetAllEventsInternal

--- FPype.Infrastructure/Services/SchedulingService.fs
+++ FPype.Infrastructure/Services/SchedulingService.fs
@@ -8,16 +8,17 @@ open FPype.Infrastructure
 type SchedulingService(ctx: MySqlContext, log: ILogger<SchedulingService>) =
     
     member _.AddSchedule(userReference, schedule) =
-        Scheduling.CreateOperations.schedule ctx log userReference schedule
+        CreateOperations.schedule ctx log userReference schedule
     
     member _.UpdateSchedule(userReference, scheduleReference, update) =
-        Scheduling.UpdateOperations.schedule ctx log userReference scheduleReference update
+        UpdateOperations.schedule ctx log userReference scheduleReference update
     
     member _.ActivateSchedule(userReference, scheduleReference) =
-        Scheduling.UpdateOperations.activateSchedule ctx log userReference scheduleReference
+        UpdateOperations.activateSchedule ctx log userReference scheduleReference
         
     member _.DeactivateSchedule(userReference, scheduleReference) =
-        Scheduling.UpdateOperations.deactivateSchedule ctx log userReference scheduleReference
+        UpdateOperations.deactivateSchedule ctx log userReference scheduleReference
         
     member _.GetSerialTipInternal() = Events.selectGlobalTip ctx
         
+    member _.GetAllEventsInternal(previousSerial) = Events.selectAllEventRecordsFromPreviousTip ctx previousSerial
