9b998c79bf86369d14a2857e790bc251db45b3b9
Max Clifford <me@mclifford.dev>
2023-09-11 18:43:31 +0100

    Added ReadOperations.allEventsInternal

--- FPype.Infrastructure/Scheduling/ReadOperations.fs
+++ FPype.Infrastructure/Scheduling/ReadOperations.fs
@@ -5,7 +5,24 @@ open Freql.MySql
 [<RequireQualifiedAccess>]
 module ReadOperations =
 
+    open Microsoft.Extensions.Logging
+    open FPype.Infrastructure.Scheduling.Models
+    open Freql.MySql
+    open FPype.Infrastructure.Configuration.Common
+    open FsToolbox.Core.Results
+    open FPype.Infrastructure.Core
+    open FPype.Infrastructure.Core.Persistence
+
+    let allEventsInternal (ctx: MySqlContext) (logger: ILogger) (previousTip: int) =
+        let rc = Events.selectAllEvents ctx previousTip
+
+        match rc.HasErrors() with
+        | true ->
+            rc.Errors
+            |> List.iter (fun e -> logger.LogError($"Failed to deserialize event. Error: {e}"))
+        | false -> ()
+
+        rc.Success |> FetchResult.Success
+
+
     ()
-
-
-
