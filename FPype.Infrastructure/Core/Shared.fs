96281e8434ebe48b09b22b033ced6b83aaaf9276
Max Clifford <me@mclifford.dev>
2023-09-11 18:36:28 +0100

    Added ResultsCollection

--- FPype.Infrastructure/Core/Shared.fs
+++ FPype.Infrastructure/Core/Shared.fs
@@ -6,11 +6,19 @@ module Shared =
     open System
     open System.Text.Json
     open FsToolbox.Core.Results
-    
+
+    type ResultCollection<'T> =
+        { Success: 'T list
+          Errors: FailureResult list }
+
+        static member Create(success: 'T list, errors: FailureResult list) = { Success = success; Errors = errors }
+
+        member rc.HasErrors() = rc.Errors.Length
+
     let createReference () = Guid.NewGuid().ToString("n")
 
     let getTimestamp () = DateTime.UtcNow
-        
+
     let toJson (value: 'T) =
         try
             JsonSerializer.Serialize value |> Ok
