9e13c583f5dd93b238ddafaa93650c0c9786fa15
Max Clifford <me@mclifford.dev>
2023-09-15 18:57:39 +0100

    Updated SchedulingService.fs

--- FPype.Infrastructure/Services/SchedulingService.fs
+++ FPype.Infrastructure/Services/SchedulingService.fs
@@ -6,26 +6,32 @@ open Freql.MySql
 open FPype.Infrastructure
 
 type SchedulingService(ctx: MySqlContext, log: ILogger<SchedulingService>) =
-    
+
     member _.AddSchedule(userReference, schedule) =
         CreateOperations.schedule ctx log userReference schedule
-    
+
     member _.UpdateSchedule(userReference, scheduleReference, update) =
         UpdateOperations.schedule ctx log userReference scheduleReference update
-    
+
     member _.ActivateSchedule(userReference, scheduleReference) =
         UpdateOperations.activateSchedule ctx log userReference scheduleReference
-        
+
     member _.DeactivateSchedule(userReference, scheduleReference) =
         UpdateOperations.deactivateSchedule ctx log userReference scheduleReference
-        
+
     member _.GetSerialTipInternal() = Events.selectGlobalTip ctx
-        
-    member _.GetAllEventsFromSerialInternal(previousSerial) = ReadOperations.allEventsInternal ctx log previousSerial
-    
+
+    member _.GetAllEventsFromSerialInternal(previousSerial) =
+        ReadOperations.allEventsInternal ctx log previousSerial
+
     member _.GetScheduleEventsFromSerialInternal(scheduleReference, previousSerial) =
         ReadOperations.scheduleEventsInternal ctx log scheduleReference previousSerial
-    
-    member _.GetAllEventsFromStartInternal() = ReadOperations.allEventsInternal ctx log -1
-    
-    member _.GetScheduleEventsFromStartInternal(scheduleReference) = ReadOperations.scheduleEventsInternal ctx log scheduleReference -1
+
+    member _.GetAllEventsFromStartInternal() =
+        ReadOperations.allEventsInternal ctx log -1
+
+    member _.GetScheduleEventsFromStartInternal(scheduleReference) =
+        ReadOperations.scheduleEventsInternal ctx log scheduleReference -1
+
+    member _.GetActiveSchedulesForUser(userReference) =
+        ReadOperations.activePipelinesForUser ctx log userReference
