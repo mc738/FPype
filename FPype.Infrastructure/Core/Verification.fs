0bf748e9819e33294bf457928a74349be20a1168
Max Clifford <me@mclifford.dev>
2023-09-18 19:37:52 +0100

    Added scheduleRun

--- FPype.Infrastructure/Core/Verification.fs
+++ FPype.Infrastructure/Core/Verification.fs
@@ -1,66 +1,66 @@
 ﻿namespace FPype.Infrastructure.Core
 
 open System
+open FsToolbox.Core.Results
 
+
+ [<RequireQualifiedAccess>]
+type VerificationResult =
+    | Success
+    | MissingPermission of Name: string
+    | WrongSubscription
+    | ItemInactive of ItemType: string
+    | Failure of FailureResult
+
+    member vr.ToResult() =
+        match vr with
+        | Success -> Ok()
+        | MissingPermission name ->
+            ({ Message = $"`{name}` permission is missing."
+               DisplayMessage = "A permission is missing."
+               Exception = None }
+            : FailureResult)
+            |> Error
+        | WrongSubscription ->
+            ({ Message = "Item's subscription is wrong"
+               DisplayMessage = "Item's subscription is wrong"
+               Exception = None }
+            : FailureResult)
+            |> Error
+        | ItemInactive itemType ->
+            ({ Message = $"{itemType} is inactive"
+               DisplayMessage = $"{itemType} is inactive"
+               Exception = None }
+            : FailureResult)
+            |> Error
+        | Failure failure -> Error failure
+
+module VerificationResult =
+
+    let chain (fn: unit -> VerificationResult) (result: VerificationResult) =
+        match result with
+        | VerificationResult.Success -> fn ()
+        | _ -> result
+
+    let toResult<'T> (value: 'T) (verificationResult: VerificationResult) =
+        verificationResult.ToResult() |> Result.map (fun _ -> value)
+
+    let verify<'T> (verifiers: (unit -> VerificationResult) list) (value: 'T) =
+        verifiers
+        |> List.fold
+            (fun r v ->
+                match r with
+                | VerificationResult.Success -> v ()
+                | _ -> r)
+            (VerificationResult.Success)
+        |> toResult value
 
 [<RequireQualifiedAccess>]
 module Verification =
 
     open FPype.Configuration.Persistence
-    open FsToolbox.Core.Results
     open FPype.Infrastructure.Core
     open FPype.Infrastructure.Core.Persistence
-    
-     [<RequireQualifiedAccess>]
-    type VerificationResult =
-        | Success
-        | MissingPermission of Name: string
-        | WrongSubscription
-        | ItemInactive of ItemType: string
-        | Failure of FailureResult
-
-        member vr.ToResult() =
-            match vr with
-            | Success -> Ok()
-            | MissingPermission name ->
-                ({ Message = $"`{name}` permission is missing."
-                   DisplayMessage = "A permission is missing."
-                   Exception = None }
-                : FailureResult)
-                |> Error
-            | WrongSubscription ->
-                ({ Message = "Item's subscription is wrong"
-                   DisplayMessage = "Item's subscription is wrong"
-                   Exception = None }
-                : FailureResult)
-                |> Error
-            | ItemInactive itemType ->
-                ({ Message = $"{itemType} is inactive"
-                   DisplayMessage = $"{itemType} is inactive"
-                   Exception = None }
-                : FailureResult)
-                |> Error
-            | Failure failure -> Error failure
-
-    module VerificationResult =
-
-        let chain (fn: unit -> VerificationResult) (result: VerificationResult) =
-            match result with
-            | VerificationResult.Success -> fn ()
-            | _ -> result
-
-        let toResult<'T> (value: 'T) (verificationResult: VerificationResult) =
-            verificationResult.ToResult() |> Result.map (fun _ -> value)
-
-        let verify<'T> (verifiers: (unit -> VerificationResult) list) (value: 'T) =
-            verifiers
-            |> List.fold
-                (fun r v ->
-                    match r with
-                    | VerificationResult.Success -> v ()
-                    | _ -> r)
-                (VerificationResult.Success)
-            |> toResult value
 
     let isActive<'T> (name: string) (testFn: 'T -> bool) (value: 'T) =
         match testFn value with
--- FPype.Infrastructure/Scheduling/CreateOperations.fs
+++ FPype.Infrastructure/Scheduling/CreateOperations.fs
@@ -1,5 +1,7 @@
 ﻿namespace FPype.Infrastructure.Scheduling
 
+open System
+
 [<RequireQualifiedAccess>]
 module CreateOperations =
 
@@ -10,7 +12,7 @@ module CreateOperations =
     open FsToolbox.Core.Results
     open FPype.Infrastructure.Core
     open FPype.Infrastructure.Core.Persistence
-    
+
     let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (schedule: NewSchedule) =
         ctx.ExecuteInTransaction(fun t ->
             Fetch.user t userReference
@@ -49,5 +51,56 @@ module CreateOperations =
                 |> ignore))
         |> toActionResult "Create schedule"
 
+    let scheduleRun
+        (ctx: MySqlContext)
+        (logger: ILogger)
+        (scheduleReference: string)
+        (basePath: string)
+        (runId: string)
+        =
+        ctx.ExecuteInTransaction(fun t ->
+            Fetch.user t Users.defaultSystemReference
+            |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById t ur.SubscriptionId)
+            |> FetchResult.chain (fun (ur, sr) psr -> ur, sr, psr) (Fetch.scheduleByReference t scheduleReference)
+            |> FetchResult.merge (fun (ur, sr, psr) pvr -> ur, sr, pvr, psr) (fun (_, _, psr) ->
+                Fetch.pipelineVersionById t psr.PipelineVersionId)
+            |> FetchResult.merge (fun (ur, sr, pvr, psr) pr -> ur, sr, pr, pvr, psr) (fun (_, _, pvr, _) ->
+                Fetch.pipelineById t pvr.PipelineId)
+            |> FetchResult.toResult
+            |> Result.bind (fun (ur, sr, pr, pvr, psr) ->
+                let verifiers =
+                    [ Verification.subscriptionIsActive sr
+                      Verification.userIsActive ur
+                      Verification.scheduleIsActive psr
+                      // These might not technically be needed but they can guard against regressions
+                      Verification.isSystemSubscription sr
+                      Verification.isSystemUser ur ]
 
-    let scheduleRun (ctx: MySqlContext) (logger: ILogger) = ()
+                VerificationResult.verify verifiers (ur, sr, pr, pvr, psr))
+            |> Result.map (fun (ur, sr, pr, pvr, psr) ->
+                let timestamp = DateTime.UtcNow
+
+                // NOTE the subscription id is the same as the pipeline. Not the system one.
+                let internalRunId =
+                    ({ Reference = runId
+                       SubscriptionId = pr.SubscriptionId
+                       PipelineVersionId = pvr.Id
+                       QueuedOn = timestamp
+                       StartedOn = None
+                       CompletedOn = None
+                       WasSuccessful = None
+                       BasePath = basePath
+                       RunBy = ur.Id }
+                    : Parameters.NewPipelineRunItem)
+                    |> Operations.insertPipelineRunItem t
+                    |> int
+
+                // NOTE the schedule run has the same reference as the run item. This could change later however.
+                ({ Reference = runId
+                   ScheduleId = psr.Id
+                   PipelineRunId = internalRunId
+                   RunOn = timestamp }
+                : Parameters.NewPipelineScheduleRun)
+                |> Operations.insertPipelineScheduleRun t
+                |> ignore))
+        |> toActionResult "Queue schedule pipeline run"
