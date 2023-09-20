f1fe23baf94603bbaff67dacf333db7644569391
Max Clifford <me@mclifford.dev>
2023-09-20 19:32:47 +0100

    Added get schedule

--- FPype.Infrastructure/Scheduling/ReadOperations.fs
+++ FPype.Infrastructure/Scheduling/ReadOperations.fs
@@ -73,29 +73,66 @@ module ReadOperations =
     let allActiveSchedules (ctx: MySqlContext) (logger: ILogger) =
         try
             Operations.selectPipelineScheduleRecords ctx [ "WHERE active = TRUE" ] []
-                |> List.choose (fun psr ->
-                    // TODO what to do if pipeline or pipeline version not found?
-                    Operations.selectPipelineVersionRecord ctx [ "WHERE id = @0" ] [ psr.PipelineVersionId ]
-                    |> Option.bind (fun pvr ->
-                        Operations.selectPipelineRecord ctx [ "WHERE id = @0" ] [ pvr.PipelineId ]
-                        |> Option.map (fun pr -> pr, pvr))
-                    |> Option.bind (fun (pr, pvr) ->
-                        Operations.selectSubscriptionRecord ctx [ "WHERE id = @0" ] [ pr.SubscriptionId ]
-                        |> Option.map (fun sr -> sr, pr, pvr))
-                    |> Option.map (fun (sr, pr, pvr) ->
+            |> List.choose (fun psr ->
+                // TODO what to do if pipeline or pipeline version not found?
+                Operations.selectPipelineVersionRecord ctx [ "WHERE id = @0" ] [ psr.PipelineVersionId ]
+                |> Option.bind (fun pvr ->
+                    Operations.selectPipelineRecord ctx [ "WHERE id = @0" ] [ pvr.PipelineId ]
+                    |> Option.map (fun pr -> pr, pvr))
+                |> Option.bind (fun (pr, pvr) ->
+                    Operations.selectSubscriptionRecord ctx [ "WHERE id = @0" ] [ pr.SubscriptionId ]
+                    |> Option.map (fun sr -> sr, pr, pvr))
+                |> Option.map (fun (sr, pr, pvr) ->
 
-                        ({ Reference = psr.Reference
-                           SubscriptionReference = sr.Reference
-                           PipelineReference = pr.Reference
-                           Pipeline = pr.Name
-                           PipelineVersionReference = pvr.Reference
-                           PipelineVersion = pvr.Version
-                           ScheduleCron = psr.ScheduleCron }
-                        : Models.ScheduleOverview)))
+                    ({ Reference = psr.Reference
+                       SubscriptionReference = sr.Reference
+                       PipelineReference = pr.Reference
+                       Pipeline = pr.Name
+                       PipelineVersionReference = pvr.Reference
+                       PipelineVersion = pvr.Version
+                       ScheduleCron = psr.ScheduleCron }
+                    : Models.ScheduleOverview)))
             |> FetchResult.Success
         with ex ->
             { Message = "Unhandled exception while fetching active schedules"
               DisplayMessage = "Error fetching active schedules"
               Exception = Some ex }
             |> FetchResult.Failure
-            
+
+    let schedule (ctx: MySqlContext) (logger: ILogger) (userReference: string) (scheduleReference: string) =
+        try
+            Fetch.user ctx userReference
+            |> FetchResult.merge (fun ur sr -> ur, sr) (fun ur -> Fetch.subscriptionById ctx ur.Id)
+            |> FetchResult.chain (fun (ur, sr) psr -> ur, sr, psr) (Fetch.scheduleByReference ctx scheduleReference)
+            |> FetchResult.merge (fun (ur, sr, psr) pvr -> ur, sr, pvr, psr) (fun (_, _, psr) ->
+                Fetch.pipelineVersionById ctx psr.PipelineVersionId)
+            |> FetchResult.merge (fun (ur, sr, pvr, psr) pr -> ur, sr, pr, pvr, psr) (fun (_, _, pvr, _) ->
+                Fetch.pipelineById ctx pvr.PipelineId)
+            |> FetchResult.toResult
+            |> Result.bind (fun (ur, sr, pr, pvr, psr) ->
+                let verifiers =
+                    [ Verification.userIsActive ur
+                      Verification.subscriptionIsActive sr
+                      Verification.subscriptionMatches sr psr.SubscriptionId
+                      Verification.subscriptionMatches sr pr.SubscriptionId
+                      // SECURITY These might not strictly be needed but a a good cover for regressions and ensure system users can not perform this operation.
+                      Verification.isNotSystemSubscription sr
+                      Verification.isNotSystemUser ur ]
+
+                VerificationResult.verify verifiers (ur, sr, pr, pvr, psr))
+            |> Result.map (fun (ur, sr, pr, pvr, psr) ->
+                ({ Reference = psr.Reference
+                   SubscriptionReference = sr.Reference
+                   PipelineReference = pr.Reference
+                   Pipeline = pr.Name
+                   PipelineVersionReference = pvr.Reference
+                   PipelineVersion = pvr.Version
+                   ScheduleCron = psr.ScheduleCron
+                   Active = sr.Active }
+                : Models.ScheduleDetails))
+            |> FetchResult.fromResult
+        with ex ->
+            { Message = "Unhandled exception while fetching active schedules"
+              DisplayMessage = "Error fetching active schedules"
+              Exception = Some ex }
+            |> FetchResult.Failure
