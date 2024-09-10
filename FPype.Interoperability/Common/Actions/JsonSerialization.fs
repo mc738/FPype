51eb54bfd579f252696f1500efe4b4b416706846
max <me@mclifford.dev>
2024-09-10 16:56:32 +0100

    Work on JsonSerialization.fs

--- FPype.Interoperability/Common/Actions/JsonSerialization.fs
+++ FPype.Interoperability/Common/Actions/JsonSerialization.fs
@@ -19,26 +19,20 @@ module JsonSerialization =
                 |> Seq.filter (fun at -> t.IsAssignableFrom(at) && at.IsClass && (at.IsAbstract |> not)))
             |> Seq.toList
 
-
         override this.Read(reader, typeToConvert, options) =
             if reader.TokenType <> JsonTokenType.StartObject then
                 raise (JsonException())
-                
+
             use doc = JsonDocument.ParseValue(&reader)
-            
+
             match doc.RootElement.TryGetProperty("actionType") with
             | true, jsonElement ->
-                match types |> List.tryFind ()
-                
-                failwith "todo"
-            | false, _ ->
-                raise (JsonException())
+                match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
+                | None -> raise (JsonException())
+                | Some value ->
+                    let jsonObject = doc.RootElement.GetRawText()
+                    JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+            | false, _ -> raise (JsonException())
 
-
-            failwith "todo"
-
-        override this.Write(writer, value, options) = failwith "todo"
-
-
-
-    ()
+        override this.Write(writer, value, options) =
+            JsonSerializer.Serialize(writer, box value, options)
