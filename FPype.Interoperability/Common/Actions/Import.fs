f84542790381d4b8e9d7df374956fde04ca09157
max <me@mclifford.dev>
2024-09-11 15:57:39 +0100

    Work on Import.fs

--- FPype.Interoperability/Common/Actions/Import.fs
+++ FPype.Interoperability/Common/Actions/Import.fs
@@ -1,17 +1,78 @@
 namespace FPype.Interoperability.Common.Actions
 
 open System.Text.Json.Serialization
+open FPype.Actions
 
 module Import =
-    
+
+    open FPype.Actions
+
     type ImportFileAction =
-            {
-                [<JsonPropertyName "path">]
-                Path: string
-                [<JsonPropertyName "dataSourceName">]
-                DataSourceName: string
-            }
-     
-            interface IPipelineAction with
-                
-                member _.ActionType = nameof(ImportFileAction)
+        { [<JsonPropertyName "path">]
+          Path: string
+          [<JsonPropertyName "dataSourceName">]
+          DataSourceName: string }
+
+        interface IPipelineAction with
+
+            [<JsonPropertyName "actionType">]
+            member this.ActionType = nameof this
+
+            member this.GetActionName() = Import.``import-file``.name
+
+            member this.ToSerializedActionParameters() =
+                ({ Path = this.Path
+                   Name = this.DataSourceName }
+                : Import.``import-file``.Parameters)
+                |> serializeAsJson
+
+    type ChunkFileAction =
+        { [<JsonPropertyName "path">]
+          Path: string
+          [<JsonPropertyName "collectionName">]
+          CollectionName: string
+          [<JsonPropertyName "chunkSize">]
+          ChunkSize: int }
+
+        interface IPipelineAction with
+
+            [<JsonPropertyName "actionType">]
+            member this.ActionType = nameof this
+
+            member this.GetActionName() = Import.``chunk-file``.name
+
+            member this.ToSerializedActionParameters() =
+                ({ Path = this.Path
+                   CollectionName = this.CollectionName
+                   ChunkSize = this.ChunkSize }
+                : Import.``chunk-file``.Parameters)
+                |> serializeAsJson
+
+    // TODO unzip-file
+
+    type HttpGetAction =
+        { [<JsonPropertyName "url">]
+          Url: string
+          [<JsonPropertyName "additionalHeaders">]
+          AdditionHeaders: StringKeyValue list
+          [<JsonPropertyName "name">]
+          Name: string
+          [<JsonPropertyName "responseType">]
+          ResponseType: string option
+          [<JsonPropertyName "collection">]
+          Collection: string option }
+
+
+        interface IPipelineAction with
+            member this.ActionType = nameof this
+
+            member this.GetActionName() = Import.``http-get``.name
+
+            member this.ToSerializedActionParameters() =
+                ({ Url = this.Url
+                   AdditionalHeaders = this.AdditionHeaders |> List.map (fun kv -> kv.Key, kv.Value) |> Map.ofList
+                   Name = this.Name
+                   ResponseType = this.ResponseType
+                   Collection = this.Collection }
+                : Import.``http-get``.Parameters)
+                |> serializeAsJson
