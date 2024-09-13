f54a6e2960a4a0ea0489a4deeb449738d095984c
max <me@mclifford.dev>
2024-09-13 13:11:34 +0100

    Added Transform.fs

---
+++ FPype.Interoperability/Common/Actions/Transform.fs
@@ -0,0 +1,23 @@
+namespace FPype.Interoperability.Common.Actions
+
+module Transform =
+
+    open System.Text.Json
+    open FsToolbox.Core
+    open System.Text.Json.Serialization
+    open FPype.Actions
+
+    type ExecuteQueryAction =
+        { [<JsonPropertyName "query">]
+          Query: ActionQueryVersion
+          [<JsonPropertyName "table">]
+          Table: ActionTableVersion }
+        
+        interface IPipelineAction with
+        
+            [<JsonPropertyName "actionType">]
+            member this.ActionType = nameof this
+            
+            member this.GetActionName() = Transform.``execute-query``.name
+            
+            member this.ToSerializedActionParameters() = failwith "todo"
