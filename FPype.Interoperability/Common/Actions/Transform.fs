b239e76cd7744d32020c431fddd855f4730ee761
max <me@mclifford.dev>
2024-09-13 14:49:09 +0100

    Work on Transform.fs

--- FPype.Interoperability/Common/Actions/Transform.fs
+++ FPype.Interoperability/Common/Actions/Transform.fs
@@ -12,12 +12,37 @@ module Transform =
           Query: ActionQueryVersion
           [<JsonPropertyName "table">]
           Table: ActionTableVersion }
-        
+
         interface IPipelineAction with
-        
+
             [<JsonPropertyName "actionType">]
             member this.ActionType = nameof this
-            
+
             member this.GetActionName() = Transform.``execute-query``.name
-            
-            member this.ToSerializedActionParameters() = failwith "todo"
+
+            member this.ToSerializedActionParameters() =
+                writeJson (
+                    Json.writeObject (fun w ->
+                        this.Table.WriteToJsonProperty("query", w)
+                        this.Table.WriteToJsonProperty("table", w))
+                )
+
+    type AggregateAction =
+        { [<JsonPropertyName "query">]
+          Query: ActionQueryVersion
+          [<JsonPropertyName "table">]
+          Table: ActionTableVersion }
+
+        interface IPipelineAction with
+
+            [<JsonPropertyName "actionType">]
+            member this.ActionType = nameof this
+
+            member this.GetActionName() = Transform.``execute-query``.name
+
+            member this.ToSerializedActionParameters() =
+                writeJson (
+                    Json.writeObject (fun w ->
+                        this.Table.WriteToJsonProperty("query", w)
+                        this.Table.WriteToJsonProperty("table", w))
+                )
