298051197c91b80980d7565e6cb41640e2dda541
max <me@mclifford.dev>
2024-09-13 12:27:01 +0100

    Work on ML.fs

--- FPype.Interoperability/Common/Actions/ML.fs
+++ FPype.Interoperability/Common/Actions/ML.fs
@@ -1,6 +1,44 @@
 namespace FPype.Interoperability.Common.Actions
 
 module ML =
+
+    open System.Text.Json
+    open FsToolbox.Core
+    open System.Text.Json.Serialization
+    open FPype.Actions
+    
+    type ITransformationType =
+        
+        abstract member TransformationType: string
+        
+    type ConcatenateTransformation =
+        {
+            [<JsonPropertyName "outputColumnName">]
+            OutputColumnName: string
+            Columns: string list
+        }
+    
+    type TrainBinaryClassificationModelAction =
+        { [<JsonPropertyName "modelName">]
+          ModelName: string
+          // TODO fix this?
+          [<JsonPropertyName "source">]
+          DataSource: string
+          [<JsonPropertyName "modelSavePath">]
+          ModelSavePath: string
+          [<JsonPropertyName "contextSeed">]
+          ContextSeed: int option }
+        
+        interface IPipelineAction with
+        
+            
+            [<JsonPropertyName "actionType">]
+            member this.ActionType = nameof this
+            
+            member this.GetActionName() = ML.``train-binary-classification-model``.name
+            member this.ToSerializedActionParameters() = failwith "todo"
+
+    
+    
     
     ()
-
