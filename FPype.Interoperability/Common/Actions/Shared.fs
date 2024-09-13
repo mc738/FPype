02c24b6e3745b4f9c5bab5f5ab5c8054e1b3ebf2
max <me@mclifford.dev>
2024-09-13 14:49:36 +0100

    Work on Shared.fs

--- FPype.Interoperability/Common/Actions/Shared.fs
+++ FPype.Interoperability/Common/Actions/Shared.fs
@@ -1,5 +1,7 @@
 namespace FPype.Interoperability.Common.Actions
 
+open System
+
 [<AutoOpen>]
 module Shared =
 
@@ -16,6 +18,52 @@ module Shared =
         abstract member ToSerializedActionParameters: unit -> string
 
         abstract member GetActionName: unit -> string
+
+    type IDateGroups =
+
+        abstract member GroupType: string
+
+        abstract member WriteToJsonValue: Writer: Utf8JsonWriter -> unit
+
+        abstract member WriteToJsonProperty: PropertyName: string * Writer: Utf8JsonWriter -> unit
+
+    type MonthDateGroup =
+        { [<JsonPropertyName "fieldName">]
+          FieldName: string
+          [<JsonPropertyName "label">]
+          Label: string
+          [<JsonPropertyName "start">]
+          Start: DateTime
+          [<JsonPropertyName "length">]
+          Length: int }
+
+        interface IDateGroups with
+
+            [<JsonPropertyName "groupType">]
+            member this.GroupType = nameof this
+
+            member this.WriteToJsonProperty(propertyName, writer) =
+                Json.writePropertyObject
+                    (fun w ->
+                        w.WriteString("type", "months")
+
+                        w.WriteString("fieldName", this.FieldName)
+                        w.WriteString("label", this.Label)
+                        w.WriteString("start", this.Start)
+                        w.WriteNumber("length", this.Length))
+                    propertyName
+                    writer
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "months")
+
+                        w.WriteString("fieldName", this.FieldName)
+                        w.WriteString("label", this.Label)
+                        w.WriteString("start", this.Start)
+                        w.WriteNumber("length", this.Length))
+                    writer
 
     type StringKeyValue =
         { [<JsonPropertyName "key">]
@@ -41,6 +89,27 @@ module Shared =
                 (fun w ->
                     w.WriteString("name", atv.Name)
                     atv.Version |> Option.iter (fun v -> w.WriteNumber("version", v)))
+                propertyName
+                writer
+
+    type ActionQueryVersion =
+        { [<JsonPropertyName "name">]
+          Name: string
+          [<JsonPropertyName "version">]
+          Version: int option }
+
+        member aqv.WriteToJsonValue(writer: Utf8JsonWriter) =
+            Json.writeObject
+                (fun w ->
+                    w.WriteString("name", aqv.Name)
+                    aqv.Version |> Option.iter (fun v -> w.WriteNumber("version", v)))
+                writer
+
+        member aqv.WriteToJsonProperty(propertyName: string, writer: Utf8JsonWriter) =
+            Json.writePropertyObject
+                (fun w ->
+                    w.WriteString("name", aqv.Name)
+                    aqv.Version |> Option.iter (fun v -> w.WriteNumber("version", v)))
                 propertyName
                 writer
 
