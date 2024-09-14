2e7a4dc7d6eb9dfa3fc101676425277d727605ba
max <me@mclifford.dev>
2024-09-14 13:32:17 +0100

    Work on JsonSerialization.fs

--- FPype.Interoperability/Common/Actions/JsonSerialization.fs
+++ FPype.Interoperability/Common/Actions/JsonSerialization.fs
@@ -3,6 +3,7 @@ namespace FPype.Interoperability.Common.Actions
 open System
 open System.Text.Json
 open System.Text.Json.Serialization
+open FPype.Interoperability.Common.Actions.ML
 
 module JsonSerialization =
 
@@ -29,9 +30,66 @@ module JsonSerialization =
             | true, jsonElement ->
                 match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
                 | None -> raise (JsonException())
-                | Some value ->
-                    let jsonObject = doc.RootElement.GetRawText()
-                    JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+                | Some value -> JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+            | false, _ -> raise (JsonException())
+
+        override this.Write(writer, value, options) =
+            JsonSerializer.Serialize(writer, box value, options)
+
+
+    type DateGroupConverter<'T when 'T :> IDateGroups>() =
+
+        inherit JsonConverter<'T>()
+
+        let types =
+            let t = typeof<'T>
+
+            AppDomain.CurrentDomain.GetAssemblies()
+            |> Seq.collect (fun a ->
+                a.GetTypes()
+                |> Seq.filter (fun at -> t.IsAssignableFrom(at) && at.IsClass && (at.IsAbstract |> not)))
+            |> Seq.toList
+
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match doc.RootElement.TryGetProperty("groupType") with
+            | true, jsonElement ->
+                match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
+                | None -> raise (JsonException())
+                | Some value -> JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+            | false, _ -> raise (JsonException())
+
+        override this.Write(writer, value, options) =
+            JsonSerializer.Serialize(writer, box value, options)
+
+    type TransformationTypeConverter<'T when 'T :> ITransformationType>() =
+
+        inherit JsonConverter<'T>()
+
+        let types =
+            let t = typeof<'T>
+
+            AppDomain.CurrentDomain.GetAssemblies()
+            |> Seq.collect (fun a ->
+                a.GetTypes()
+                |> Seq.filter (fun at -> t.IsAssignableFrom(at) && at.IsClass && (at.IsAbstract |> not)))
+            |> Seq.toList
+
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match doc.RootElement.TryGetProperty("transformationType") with
+            | true, jsonElement ->
+                match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
+                | None -> raise (JsonException())
+                | Some value -> JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
             | false, _ -> raise (JsonException())
 
         override this.Write(writer, value, options) =
