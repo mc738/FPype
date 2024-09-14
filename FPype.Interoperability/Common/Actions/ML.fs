bd8a125be1ca8874bebd9c3c9ff11d297c742c48
max <me@mclifford.dev>
2024-09-14 13:31:54 +0100

    Work on ML.fs

--- FPype.Interoperability/Common/Actions/ML.fs
+++ FPype.Interoperability/Common/Actions/ML.fs
@@ -6,18 +6,127 @@ module ML =
     open FsToolbox.Core
     open System.Text.Json.Serialization
     open FPype.Actions
-    
+
     type ITransformationType =
-        
+
         abstract member TransformationType: string
-        
+
+        abstract member WriteToJsonValue: Writer: Utf8JsonWriter -> unit
+
     type ConcatenateTransformation =
-        {
-            [<JsonPropertyName "outputColumnName">]
-            OutputColumnName: string
-            Columns: string list
-        }
-    
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string
+          [<JsonPropertyName "columns">]
+          Columns: string list }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "copy-columns")
+                        w.WriteString("outputColumnName", this.OutputColumnName)
+                        Json.writeArray (fun aw -> this.Columns |> List.iter aw.WriteStringValue) "columns" w)
+                    writer
+
+    type CopyColumnsTransformation =
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string
+          [<JsonPropertyName "inputColumnName">]
+          InputColumnName: string }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "copy-columns")
+                        w.WriteString("outputColumnName", this.OutputColumnName)
+                        w.WriteString("inputColumnName", this.InputColumnName))
+                    writer
+
+    type FeaturizeTextTransformation =
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string
+          [<JsonPropertyName "inputColumnName">]
+          InputColumnName: string }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "featurize-text")
+                        w.WriteString("outputColumnName", this.OutputColumnName)
+                        w.WriteString("inputColumnName", this.InputColumnName))
+                    writer
+
+    type MapValueToKeyTransformation =
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string
+          [<JsonPropertyName "inputColumnName">]
+          InputColumnName: string }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "map-value-to-key")
+                        w.WriteString("outputColumnName", this.OutputColumnName)
+                        w.WriteString("inputColumnName", this.InputColumnName))
+                    writer
+
+    type NormalizeMeanVarianceTransformation =
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "normalize-mean-variance")
+                        w.WriteString("outputColumnName", this.OutputColumnName))
+                    writer
+
+    type OneHotEncodingTransformation =
+        { [<JsonPropertyName "outputColumnName">]
+          OutputColumnName: string
+          [<JsonPropertyName "inputColumnName">]
+          InputColumnName: string }
+
+        interface ITransformationType with
+
+            [<JsonPropertyName "transformationType">]
+            member this.TransformationType = nameof this
+
+            member this.WriteToJsonValue(writer) =
+                Json.writeObject
+                    (fun w ->
+                        w.WriteString("type", "one-hot-encoding")
+                        w.WriteString("outputColumnName", this.OutputColumnName)
+                        w.WriteString("inputColumnName", this.InputColumnName))
+                    writer
+                    
+                    
+    type 
+
     type TrainBinaryClassificationModelAction =
         { [<JsonPropertyName "modelName">]
           ModelName: string
@@ -28,17 +137,19 @@ module ML =
           ModelSavePath: string
           [<JsonPropertyName "contextSeed">]
           ContextSeed: int option }
-        
+
         interface IPipelineAction with
-        
-            
+
+
             [<JsonPropertyName "actionType">]
             member this.ActionType = nameof this
-            
-            member this.GetActionName() = ML.``train-binary-classification-model``.name
+
+            member this.GetActionName() =
+                ML.``train-binary-classification-model``.name
+
             member this.ToSerializedActionParameters() = failwith "todo"
 
-    
-    
-    
+
+
+
     ()
