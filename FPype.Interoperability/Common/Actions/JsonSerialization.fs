059066eb6ba682944d44a2f227b911b3376e1784
max <me@mclifford.dev>
2024-09-15 13:47:19 +0100

    Work on JsonSerialization.fs

--- FPype.Interoperability/Common/Actions/JsonSerialization.fs
+++ FPype.Interoperability/Common/Actions/JsonSerialization.fs
@@ -152,3 +152,62 @@ module JsonSerialization =
 
         override this.Write(writer, value, options) =
             JsonSerializer.Serialize(writer, box value, options)
+
+    type RegressionTrainerSettingsConverter<'T when 'T :> IRegressionTrainerSettings>() =
+
+        inherit JsonConverter<'T>()
+
+        let types =
+            let t = typeof<'T>
+
+            AppDomain.CurrentDomain.GetAssemblies()
+            |> Seq.collect (fun a ->
+                a.GetTypes()
+                |> Seq.filter (fun at -> t.IsAssignableFrom(at) && at.IsClass && (at.IsAbstract |> not)))
+            |> Seq.toList
+
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match doc.RootElement.TryGetProperty("trainerType") with
+            | true, jsonElement ->
+                match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
+                | None -> raise (JsonException())
+                | Some value -> JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+            | false, _ -> raise (JsonException())
+
+        override this.Write(writer, value, options) =
+            JsonSerializer.Serialize(writer, box value, options)
+
+    
+    type MulticlassClassificationTrainerSettingsConverter<'T when 'T :> IMulticlassTrainerSettings>() =
+
+        inherit JsonConverter<'T>()
+
+        let types =
+            let t = typeof<'T>
+
+            AppDomain.CurrentDomain.GetAssemblies()
+            |> Seq.collect (fun a ->
+                a.GetTypes()
+                |> Seq.filter (fun at -> t.IsAssignableFrom(at) && at.IsClass && (at.IsAbstract |> not)))
+            |> Seq.toList
+
+        override this.Read(reader, typeToConvert, options) =
+            if reader.TokenType <> JsonTokenType.StartObject then
+                raise (JsonException())
+
+            use doc = JsonDocument.ParseValue(&reader)
+
+            match doc.RootElement.TryGetProperty("trainerType") with
+            | true, jsonElement ->
+                match types |> List.tryFind (fun t -> t.Name = jsonElement.GetString()) with
+                | None -> raise (JsonException())
+                | Some value -> JsonSerializer.Deserialize(doc.RootElement.GetRawText(), value, options) :?> 'T
+            | false, _ -> raise (JsonException())
+
+        override this.Write(writer, value, options) =
+            JsonSerializer.Serialize(writer, box value, options)
